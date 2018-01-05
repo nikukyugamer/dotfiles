dead8eec2a64237f0a7e2d7aa49cdbac80579646
corselia <takiya@toran.sakura.ne.jp>
2018-01-05 20:08:37 +0900

    [add] add rbenv.sh
    add rbenv shell script

---
+++ rbenv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+cd && rbevn install 2.4.3
