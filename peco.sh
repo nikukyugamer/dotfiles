a057a4dc8bb9a9ca6f9390f9799e377cd90c996c
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-01-06 20:16:47 +0900

    [update] update shell scripts
    add +x auth

--- peco.sh
+++ peco.sh
@@ -4,4 +4,4 @@
 wget https://github.com/peco/peco/releases/download/v0.5.2/peco_linux_amd64.tar.gz
 tar -zxvf peco_linux_amd64.tar.gz
 sudo cp peco_linux_amd64/peco /usr/local/bin/peco
-rm peco_linux_amd64.tar.gz
+rm -r peco_linux_amd64.tar.gz
