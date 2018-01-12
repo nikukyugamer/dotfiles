8268c9d0a1cdba1d8f3d2a810f964d2ad82ae824
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-01-12 16:38:10 +0900

    [update] update rbenv.sh
    add necessary libraries and shellrc

--- rbenv.sh
+++ rbenv.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+sudo yum install -y gcc openssl-devel readline-devel zlib-devel
+
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-cd && rbenv install 2.4.3
+
+# if .bashrc
+# sed -e '$ a export PATH=$HOME/.rbenv/bin:$PATH' -i ~/.bashrc
+# sed -e '$ a eval "$(rbenv init -)"' -i ~/.bashrc↲
+# source ~/.bashrc
+
+# if .zshrc
+sed -e '$ a export PATH=$HOME/.rbenv/bin:$PATH' -i ~/.zshrc
+sed -e '$ a eval "$(rbenv init -)"' -i ~/.zshrc↲
+source ~/.zshrc
+
+rbenv install 2.5.0
+
