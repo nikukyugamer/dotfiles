3c9a6db86bae6796fe964ff50fd5e0f486f2804b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-01-12 16:26:04 +0900

    [modify] modify rbenv.sh
    modify typo from rbevn to rbenv

--- rbenv.sh
+++ rbenv.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-cd && rbevn install 2.4.3
+cd && rbenv install 2.4.3
