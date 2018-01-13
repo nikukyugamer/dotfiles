909712bb78af66a0d3d6bc71dc991a33d5b37c6b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-01-14 01:23:17 +0900

    [update] update rbenv.sh
    add global install

--- rbenv.sh
+++ rbenv.sh
@@ -6,3 +6,4 @@ git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 rbenv install 2.5.0
+rbenv global 2.5.0
