4ad3bf85c0675ec5fc9ba676b5836a0619f3e717
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-01-14 01:26:18 +0900

    [modify] modify about node
    not shell script but description

---
+++ how_to_install_node.md
@@ -0,0 +1,5 @@
+$ nvm ls-remote
+$ nvm install 9.4.0
+$ nvm alias default 9.4.0
+
+$ npm install -g yarn
--- install_node_npm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-~/.nvm/nvm.sh install 9.4.0
-~/.nvm/nvm.sh alias default 9.4.0
-
-npm install -g yarn
