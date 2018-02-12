7f3267c12e9ea71fb7391a61d0728ea14b9f86a3
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-02-12 09:52:47 +0900

    [update] update env-script
    add notification of required libraries to env-script

--- install_rbenv_ndenv_pyenv.sh
+++ install_rbenv_ndenv_pyenv.sh
@@ -7,3 +7,9 @@ git clone https://github.com/riywo/ndenv ~/.ndenv
 git clone https://github.com/riywo/node-build.git $(ndenv root)/plugins/node-build
 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+# [Ubuntu]
+# (Build Ruby)
+# $ sudo apt-get install gcc build-essentiallibssl-dev libreadline-dev zlib1g-dev
+# (Build Python)
+# $ sudo apt-get install libbz2-dev libreadline-dev libsqlite3-dev
