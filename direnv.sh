22e088933d1b37419f6db20405ebac9dea059d2a
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-02-12 12:28:08 +0900

    [update] update direnv.sh
    update how to install direnv (linux64 and macOS)

--- direnv.sh
+++ direnv.sh
@@ -4,8 +4,10 @@
 
 # wget https://github.com/direnv/direnv/releases/download/v2.14.0/direnv.linux-amd64
 # mv direnv.linux-amd64 direnv
+# chmod +x direnv
 # sudo mv direnv /usr/local/bin
 
 # wget https://github.com/direnv/direnv/releases/download/v2.14.0/direnv.darwin-amd64
 # mv direnv.darwin-amd64 direnv
+# chmod +x direnv
 # sudo mv direnv /usr/local/bin
