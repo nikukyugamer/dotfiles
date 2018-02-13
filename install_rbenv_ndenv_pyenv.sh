b3db236799fe6fbd22bc4b9d04d691e577d9694a
corselia <takiya@toran.sakura.ne.jp>
2018-02-13 17:41:08 +0900

    [update] update how to install pyenv
    use pyenv installer

--- install_rbenv_ndenv_pyenv.sh
+++ install_rbenv_ndenv_pyenv.sh
@@ -6,8 +6,9 @@ git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-bu
 git clone https://github.com/riywo/ndenv ~/.ndenv
 git clone https://github.com/riywo/node-build.git $(ndenv root)/plugins/node-build
 
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+# git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+# git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+# curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 
 # [Ubuntu]
 # (Build Ruby)
