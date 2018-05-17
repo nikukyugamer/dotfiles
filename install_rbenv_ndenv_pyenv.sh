73ff7782f38fb5c5e3f466d81a37ded665986ece
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-05-17 12:48:15 +0900

    [update] fix a bit

--- install_rbenv_ndenv_pyenv.sh
+++ install_rbenv_ndenv_pyenv.sh
@@ -8,3 +8,4 @@ git clone https://github.com/riywo/node-build.git $(ndenv root)/plugins/node-bui
 
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+
