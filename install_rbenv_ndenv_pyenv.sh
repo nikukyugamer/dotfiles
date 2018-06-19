e5baab5c4d2a86e0c4d5376c7c6b29b62fb639aa
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-19 23:32:08 +0900

    [update] pyenv-virtualenv already exists so commented out

--- install_rbenv_ndenv_pyenv.sh
+++ install_rbenv_ndenv_pyenv.sh
@@ -7,4 +7,4 @@ git clone https://github.com/riywo/ndenv ~/.ndenv
 git clone https://github.com/riywo/node-build.git $(ndenv root)/plugins/node-build
 
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+# git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
