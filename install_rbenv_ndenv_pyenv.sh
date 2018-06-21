09324594d1b653335867269381e4cb102771c208
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-22 08:46:13 +0900

    [update] as ndenv and pyenv, not relative path but absolute path

--- install_rbenv_ndenv_pyenv.sh
+++ install_rbenv_ndenv_pyenv.sh
@@ -4,7 +4,7 @@ git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 git clone https://github.com/riywo/ndenv ~/.ndenv
-git clone https://github.com/riywo/node-build.git $(ndenv root)/plugins/node-build
+git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
 
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
