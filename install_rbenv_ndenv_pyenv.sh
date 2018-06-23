b6eda047852329c35ff3e325934a2e2257261c0a
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-23 10:12:27 +0900

    [update] remove unnecessary row

--- install_rbenv_ndenv_pyenv.sh
+++ install_rbenv_ndenv_pyenv.sh
@@ -7,4 +7,3 @@ git clone https://github.com/riywo/ndenv ~/.ndenv
 git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
 
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
