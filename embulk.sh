2f6582fd32c27c6c930939b1ea0f2787a560f5df
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-07-07 16:17:02 +0900

    [update] update embulk.sh a bit

--- embulk.sh
+++ embulk.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
+# https://github.com/embulk/embulk
 curl --create-dirs -o ~/.embulk/bin/embulk -L "https://dl.embulk.org/embulk-latest.jar"
 chmod +x ~/.embulk/bin/embulk
-# echo 'export PATH="$HOME/.embulk/bin:$PATH"' >> ~/.bashrc
-# source ~/.bashrc
