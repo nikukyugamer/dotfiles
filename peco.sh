59bb98cf0cbe779a427a7cdf5254410ffe53498f
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-10-12 05:10:02 +0000

    add removing peco tgz file logic

--- peco.sh
+++ peco.sh
@@ -4,3 +4,4 @@ wget https://github.com/peco/peco/releases/download/v0.5.3/peco_linux_amd64.tar.
 tar -zxvf peco_linux_amd64.tar.gz
 sudo cp peco_linux_amd64/peco /usr/local/bin/peco
 rm -r -f peco_linux_amd64
+rm peco_linux_amd64.tar.gz
