1e3584235fd2f27de7ad5efe56ce933382938392
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-10-31 06:14:34 +0900

    Add Yarn for system global

---
+++ yarn_system_global.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xe
+
+# Ubuntu
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update && sudo apt-get install --no-install-recommends yarn
+
+exit 0
+
