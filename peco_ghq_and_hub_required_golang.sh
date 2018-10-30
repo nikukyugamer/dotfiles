2e61982a0fbe0a3e19392d73b718c667bd5c3c21
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-10-31 06:20:05 +0900

    Refactoring

--- peco_ghq_and_hub_required_golang.sh
+++ peco_ghq_and_hub_required_golang.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -e
 
+echo 'install peco...'
 go get github.com/peco/peco/cmd/peco
+echo 'done'
+
+echo 'install ghq...'
 go get github.com/motemen/ghq
+echo 'done'
+
+echo 'install hub...'
 go get github.com/github/hub
+echo 'done'
 
 exit 0
 
