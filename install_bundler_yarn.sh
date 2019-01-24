bc1b72563c3b7e3222fdef1f431b3374b226a044
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-01-24 12:37:18 +0900

    Add installer of Ruby, Node, Python, Golang, Bundler and Yarn

---
+++ install_bundler_yarn.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xe
+
+# With rbenv and ndenv
+gem install rbenv
+npm install -g yarn
+
+exit 0
+
---
+++ install_ruby_node_python_golang.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -xe
+
+# The latest version on 1/24/2019
+rbenv install 2.5.3 && rbenv install 2.6.0 && ndenv install v11.7.0 && pyenv install 3.7.2 && goenv install 1.11.4
+
+exit 0
+
