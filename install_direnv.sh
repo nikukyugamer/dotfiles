32e27fe022e1ba5db94e81a74165f829f2bdde7b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-01-25 16:58:49 +0900

    Update direnv installer

--- install_direnv.sh
+++ install_direnv.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -xe
 
-# Golang is required
-git clone http://github.com/zimbatm/direnv
-cd direnv
-sudo make install
-cd ..
-sudo rm -rf direnv
+# If you install direnv via make install, it's necessary for root to execute go command
+# This condition is severe so this script supports installation via downloading binary directly
+# Note: Please confirm the latest binary version
+# https://github.com/direnv/direnv/releases
+
+wget https://github.com/direnv/direnv/releases/download/v2.19.0/direnv.linux-amd64
+mv direnv.linux-amd64 direnv
+chmod +x direnv
+sudo mv direnv /usr/local/bin
 
 exit 0
