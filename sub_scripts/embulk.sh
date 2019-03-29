e34f3b9c1d0f35e5e9194dd6f83b162c1d9ea9df
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-03-29 19:37:55 +0900

    add-embulk-plugin-installation

--- sub_scripts/embulk.sh
+++ sub_scripts/embulk.sh
@@ -3,3 +3,13 @@
 # https://github.com/embulk/embulk
 curl --create-dirs -o ~/.embulk/bin/embulk -L "https://dl.embulk.org/embulk-latest.jar"
 chmod +x ~/.embulk/bin/embulk
+
+~/.embulk/bin/embulk gem install embulk-input-postgresql
+~/.embulk/bin/embulk gem install embulk-output-postgresql
+~/.embulk/bin/embulk gem install embulk-input-mysql
+~/.embulk/bin/embulk gem install embulk-output-mysql
+~/.embulk/bin/embulk gem install embulk-filter-column
+~/.embulk/bin/embulk gem install embulk-filter-ruby_proc
+
+exit 0
+
