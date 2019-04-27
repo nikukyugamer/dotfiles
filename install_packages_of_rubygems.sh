942db03f22df661211c145fe00d633e4b8e42ed3
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-04-27 17:42:11 +0900

    Modify installer of RubyGems because if MySQL version is 8, installing mysql2 gem is failed

--- install_packages_of_rubygems.sh
+++ install_packages_of_rubygems.sh
@@ -2,7 +2,11 @@
 
 gem install rubocop
 gem install rubocop-performance
-gem install mysql2
+
+# gem install mysql2
+# if MySQL version is 8, on macOS,
+# $ gem install mysql2 -- --local build.mysql2 "--with-ldflags=-L/usr/local/opt/openssl/lib"
+
 gem install pg
 gem install itamae
 gem install ridgepole
