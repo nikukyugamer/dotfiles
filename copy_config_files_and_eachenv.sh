fafb794c0092d245bf19fc8a0bb3ffdc3fa99083
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-05-06 19:17:29 +0900

    modify-copy-command-sh-for-show-log (#31)

--- copy_config_files_and_eachenv.sh
+++ copy_config_files_and_eachenv.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 cp .zshrc.eachenv.should_cp_to_home_directory ~/.zshrc
 cp .vimrc.should_cp_to_home_directory ~/.vimrc
 cp .gitconfig.should_cp_to_home_directory ~/.gitconfig
 
+exit 0
