af50d05431479ad3cfe0d5addb0cef1dac0c7d13
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-05-08 20:33:39 +0900

    add-pip-packages-for-pgcli (#33)

--- install_packages_of_pip.sh
+++ install_packages_of_pip.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+echo 'Upgrade pip'
+pip install --upgrade pip
+echo 'Done!'
+
 echo 'Install mycli command...'
 pip install mycli
 echo 'Done!'
 
 echo 'Install pgcli command...'
 pip install pgcli
+pip install psycopg2-binary
 echo 'Done!'
 
 echo 'Install google-images-download command...'
