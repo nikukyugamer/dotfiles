51d317d4d7a733248384b5c5828b0b0d2975d910
corselia <takiya@toran.sakura.ne.jp>
2019-06-02 16:43:06 +0900

    add-postgresql11-installer

---
+++ system_global/install_postgresql_11.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xe
+
+sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y postgresql
+
+exit 0
