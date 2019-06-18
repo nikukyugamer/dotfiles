1fbee789b7ef62decfbd03829495d4779854143d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-06-18 10:18:15 +0900

    Add 'dup' command in Golang

--- install_and_update_packages_of_golang.sh
+++ install_and_update_packages_of_golang.sh
@@ -4,73 +4,81 @@ echo 'Init directories...'
 mkdir ~/.go
 mkdir ~/.ghq
 
+echo 'Install/Update dep'
+go get -u github.com/golang/dep/cmd/dep
+echo 'dep Done.'
+
+echo 'Install/Update Terminal Timer...'
+go get -u github.com/drgrib/ttimer
+echo 'Terminal Timer Done.'
+
 echo 'Install/Update Tokyo Amesh...'
-go get -u -v github.com/otiai10/amesh
+go get -u github.com/otiai10/amesh
 echo 'Tokyo Amesh Done.'
 
 echo 'Install/Update peco...'
 go get -u github.com/peco/peco/cmd/peco
-echo 'Done'
+echo 'peco Done'
 
 echo 'Install/Update ghq...'
 go get -u github.com/motemen/ghq
-echo 'Done'
+echo 'ghq Done'
 
 echo 'Install/Update hub...'
 go get -u github.com/github/hub
-echo 'Done'
+echo 'hub Done'
 
 echo 'Install/Update gomi...'
 go get -u github.com/b4b4r07/gomi/...
-echo 'Done'
+echo 'gomi Done'
 
 echo 'Install/Update gore...'
 go get -u github.com/motemen/gore/cmd/gore
-echo 'Done'
+echo 'gore Done'
 
 echo 'Install/Update mdempsky/gocode...'
 go get -u github.com/mdempsky/gocode   # for code completion
-echo 'Done'
+echo 'gocode Done'
 
 echo 'Install/Update pp...'
 go get -u github.com/k0kubun/pp        # or github.com/davecgh/go-spew/spew
-echo 'Done'
+echo 'pp Done'
 
 echo 'Install/Update godoc...'
 go get -u golang.org/x/tools/cmd/godoc # for using with the :doc colon-command
-echo 'Done'
+echo 'godoc Done'
 
 echo 'Install/Update go-symbols...'
 go get -u github.com/acroca/go-symbols      # for VSCode
-echo 'Done'
+echo 'go-symbols Done'
 
 echo 'Install/Update guru...'
 go get -u golang.org/x/tools/cmd/guru       # for VSCode
-echo 'Done'
+echo 'guru Done'
 
 echo 'Install/Update gorename...'
 go get -u golang.org/x/tools/cmd/gorename   # for VSCode
-echo 'Done'
+echo 'gorename Done'
 
 echo 'Install/Update dlv...'
 go get -u github.com/go-delve/delve/cmd/dlv # for VSCode
-echo 'Done'
+echo 'dlv Done'
 
 # Duplicated? -> (github.com/mdempsky/gocode)
 echo 'Install/Update stamblerre/gocode...'
 go get -u github.com/stamblerre/gocode      # for VSCode
-echo 'Done'
+echo 'gocode Done'
 
 echo 'Install/Update golint...'
 go get -u golang.org/x/lint/golint          # for VSCode
-echo 'Done'
+echo 'golint Done'
 
 echo 'Install/Update jpcal...'
 go get -u github.com/y-yagi/jpcal
-echo 'Done'
+echo 'jpcal Done'
 
 echo 'Install/Update ojichat...'
 go get -u github.com/greymd/ojichat
-echo 'Done'
+echo 'ojichat Done'
 
 exit 0
