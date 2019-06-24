9eed49aa13bfb726d91397f4fab78831b6b9d5bc
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-06-24 10:26:05 +0900

    Add HTTPie as pip

--- install_and_upgrade_packages_of_pip.sh
+++ install_and_upgrade_packages_of_pip.sh
@@ -2,23 +2,27 @@
 
 echo 'Upgrade pip'
 pip install --upgrade pip
-echo 'Done!'
+echo 'upgrade pip Done!'
 
 echo 'Install and Upgrade mycli command...'
 pip install --upgrade mycli
-echo 'Done!'
+echo 'mycli Done!'
 
 echo 'Install and Upgrade pgcli command...'
 pip install --upgrade pgcli
 pip install --upgrade psycopg2-binary
-echo 'Done!'
+echo 'pgcli Done!'
 
 echo 'Install and Upgrade google-images-download command...'
 pip install --upgrade google-images-download
-echo 'Done!'
+echo 'google-images-download Done!'
 
 echo 'Install and Upgrade youtube-dl...'
 pip install --upgrade youtube-dl
-echo 'Done!'
+echo 'youtube-dl Done!'
+
+echo 'Install and Upgrade HTTPie'
+pip install --upgrade httpie
+echo 'HTTPie Done!'
 
 exit 0
