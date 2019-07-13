d38734e12603f6664fc9c18b529a31653bffec07
corselia <takiya@toran.sakura.ne.jp>
2019-07-13 16:10:40 +0900

    Add installing capture-website-cli script and refactor a bit

--- install_packages_of_npm.sh
+++ install_packages_of_npm.sh
@@ -1,16 +1,44 @@
 #!/bin/bash -xe
 
+echo 'Install npm...'
 npm install -g npm
+echo 'DONE: Install npm'
+
+echo 'Install yarn...'
 npm install -g yarn
+echo 'DONE: Install yarn'
 
 # Not recommend to install globally
 # npm install -g eslint
+# npm install -g vue
+# npm install -g prettier
+
+#     "eslint": "^5.12.1",
+#     "eslint-config-standard": "^12.0.0",
+#     "eslint-plugin-import": "^2.14.0",
+#     "eslint-plugin-node": "^8.0.1",
+#     "eslint-plugin-promise": "^4.0.1",
+#     "eslint-plugin-standard": "^4.0.0",
+#     "eslint-plugin-vue": "^5.1.0"
+
+echo 'Install jshint...'
 npm install -g jshint
+echo 'DONE: Install jshint'
+
+echo 'Install js-beautify...'
 npm install -g js-beautify
+echo 'DONE: Install js-beautify'
+
+echo 'Install typescript...'
 npm install -g typescript
+echo 'DONE: Install typescript'
 
 # バージョン依存がひどい
 # npm install -g terminalizer
+
+echo 'Install capture-website-cli...'
+npm install -g capture-website-cli
+echo 'DONE: Install capture-website-cli'
 
 exit 0
 
