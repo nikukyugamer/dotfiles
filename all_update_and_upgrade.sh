c9de334a93bce29cdbc818d9d4e3aca75a45d6f4
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-07-31 03:41:37 +0900

    Add 'All in one script for update and upgrade'

---
+++ all_update_and_upgrade.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -xe
+
+./install_and_update_packages_of_golang.sh
+./install_and_upgrade_packages_of_pip.sh
+./install_and_upgrade_packages_of_rust.sh
+
+# ./install_packages_of_npm.sh
+# ./install_packages_of_rubygems.sh
+
+./update_cli_tools.sh
+./update_rbenv_nodenv_pyenv_goenv_jenv.sh
+
+# 対話あり
+./update_fzf.sh
+
+exit 0
+
