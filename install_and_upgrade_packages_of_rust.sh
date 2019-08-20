2fb90e68c10376dc6fb2132018d805c87a0ab072
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-08-20 13:12:06 +0900

    Add useful Rust's three commands

--- install_and_upgrade_packages_of_rust.sh
+++ install_and_upgrade_packages_of_rust.sh
@@ -6,9 +6,19 @@ echo 'bat Done.'
 echo 'If you would like to show without line numbers, use "-p" option'
 
 echo 'Install ripgrep...'
-cargo install --force ripgrep_all
+cargo install --force ripgrep
 echo 'ripgrep Done.'
 echo 'Usage: $ rga "hello" demo/'
+
+echo 'Install fd(fd-find)...'
+cargo install --force fd-find
+echo 'fd(fd-find) Done.'
+echo 'Usage: $ fd "^x.*rc$"'
+
+echo 'Install exa...'
+cargo install --force exa
+echo 'exa Done.'
+echo 'Usage: $ exa -Tl'
 
 exit 0
 
