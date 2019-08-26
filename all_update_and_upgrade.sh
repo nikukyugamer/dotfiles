76d2d7b2443605864a6174324415400f50c82520
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-08-26 10:58:55 +0900

    Add 'all npm install shell script' to 'all_update_and_upgrade' shell script

--- all_update_and_upgrade.sh
+++ all_update_and_upgrade.sh
@@ -3,15 +3,16 @@
 ./install_and_upgrade_packages_of_golang.sh
 ./install_and_upgrade_packages_of_pip.sh
 ./install_and_upgrade_packages_of_rust.sh
+install_and_upgrade_packages_of_npm.sh
 
-# ./install_packages_of_npm.sh
 # ./install_packages_of_rubygems.sh
 
-./update_cli_tools.sh
 ./update_rbenv_nodenv_pyenv_goenv_jenv.sh
 
 # 対話あり
 ./update_fzf.sh
+
+./update_cli_tools.sh
 
 exit 0
 
