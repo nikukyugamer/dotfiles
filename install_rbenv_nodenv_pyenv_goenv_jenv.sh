571763e76fb0b95c637f9de71c0b4521f481409c
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-10-05 11:06:14 +0900

    Modify an installation command for 'pyenv'

--- install_rbenv_nodenv_pyenv_goenv_jenv.sh
+++ install_rbenv_nodenv_pyenv_goenv_jenv.sh
@@ -6,7 +6,9 @@ git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-bu
 git clone https://github.com/nodenv/nodenv.git ~/.nodenv
 git clone https://github.com/nodenv/node-build.git ~/.nodenv/plugins/node-build
 
-curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+# ~/.anyenv 配下に入るのでだめ
+# curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
 git clone https://github.com/syndbg/goenv.git ~/.goenv
 
