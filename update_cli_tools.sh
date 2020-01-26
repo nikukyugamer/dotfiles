c358a57d176710948ab55e76b00814d0384504dd
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-01-26 10:13:57 +0900

    arm では CircleCI の CLI は動作しないため、OS ごとの分岐条件を書いた

--- update_cli_tools.sh
+++ update_cli_tools.sh
@@ -1,10 +1,26 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 heroku update
 heroku plugins:install heroku-accounts
 
-# Ubuntu では sudo が必要かも
-echo "If you failed install circleci CLI, you must 'sudo circleci update'"
-circleci update
+UNAME=`uname -a`
+if [ "`echo $UNAME | grep Darwin`" ]; then
+  echo 'Update circleci...'
+  circleci update
+elif [ "`echo $UNAME | grep arm`" ]; then
+  # 現状、arm では動作しない
+  # circleci update
+  echo 'Skip "circleci update"'
+elif [ "`echo $UNAME | grep Ubuntu`" ]; then
+  echo 'Update circleci...'
+  circleci update
+elif [ "`echo $UNAME | grep el7`" ]; then
+  echo 'Update circleci...'
+  circleci update
+else
+  # echo 'What OS are you using?'
+  echo 'Skip "circleci update"'
+fi
 
 exit 0
+
