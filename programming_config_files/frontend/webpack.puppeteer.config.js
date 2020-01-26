f49fddf059aa5a5eaa662925c057d76ffe1ee58d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-01-26 21:51:57 +0900

    Puppeteer を webpack でビルドするときに ws エラーを出さないための一つの設定ファイルを置いた

---
+++ programming_config_files/frontend/webpack.puppeteer.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/entry.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  node: {
+    fs: 'empty'
+  },
+  externals: {
+    puppeteer: 'require("puppeteer")'
+  }
+};
