63aab169abc11117a39a8c5244013a931bf5c231
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-02-12 10:48:45 +0900

    Sampler のインストールスクリプトを追加した

---
+++ system_global/install_sampler.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+
+# macOS
+# brew install sampler
+# or
+# sudo curl -Lo /usr/local/bin/sampler https://github.com/sqshq/sampler/releases/download/v1.1.0/sampler-1.1.0-darwin-amd64
+# sudo chmod +x /usr/local/bin/sampler
+
+# Linux
+# sudo wget https://github.com/sqshq/sampler/releases/download/v1.1.0/sampler-1.1.0-linux-amd64 -O /usr/local/bin/sampler
+# sudo chmod +x /usr/local/bin/sampler
+
+exit 0
