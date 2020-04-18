d7331a1dab87a73f3c0c326c64bfe8c4dbba6627
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-04-18 22:26:52 +0900

    Xenvのインストールスクリプトに、何をインストールしているかを分かりやすくするためのecho出力テキストを追加した

--- update_rbenv_nodenv_pyenv_goenv_jenv.sh
+++ update_rbenv_nodenv_pyenv_goenv_jenv.sh
@@ -1,21 +1,57 @@
 #!/bin/bash
 
+echo '----------'
+
+echo '~/.rbenv'
 cd ~/.rbenv && git pull
+echo '----------'
+
+echo '~/.rbenv/plugins/ruby-build'
 cd ~/.rbenv/plugins/ruby-build && git pull
+echo '----------'
 
+echo '~/.pyenv/plugins/pyenv-doctor'
 cd ~/.pyenv/plugins/pyenv-doctor && git pull
+echo '----------'
+
+echo '~/.pyenv/plugins/pyenv-installer'
 cd ~/.pyenv/plugins/pyenv-installer && git pull
+echo '----------'
+
+echo '~/.pyenv/plugins/pyenv-update'
 cd ~/.pyenv/plugins/pyenv-update && git pull
+echo '----------'
+
+echo '~/.pyenv/plugins/pyenv-virtualenv'
 cd ~/.pyenv/plugins/pyenv-virtualenv && git pull
+echo '----------'
+
+echo '~/.pyenv/plugins/pyenv-which-ext'
 cd ~/.pyenv/plugins/pyenv-which-ext && git pull
+echo '----------'
+
+echo '~/.pyenv/plugins/python-build'
 cd ~/.pyenv/plugins/python-build && git pull
+echo '----------'
 
+echo '~/.nodenv'
 cd ~/.nodenv && git pull
+echo '----------'
+
+echo '~/.nodenv/plugins/node-build'
 cd ~/.nodenv/plugins/node-build && git pull
+echo '----------'
 
+echo '~/.goenv'
 cd ~/.goenv && git pull
-cd ~/.goenv/plugins/go-build && git pull
+echo '----------'
 
+echo '~/.goenv/plugins/go-build'
+cd ~/.goenv/plugins/go-build && git pull
+echo '----------'
+
+echo '~/.jenv'
 cd ~/.jenv && git pull
+echo '----------'
 
 exit 0
