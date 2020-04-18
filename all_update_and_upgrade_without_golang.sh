331f70e679929caf8810302599c206646e891afb
TAKIYA Osamu <takiya@toran.sakura.ne.jp>
2020-04-18 16:00:00 +0900

    cargoの高速化に伴い、インストールスクリプトを整理した (#70)
    * cargoの高速化に伴い、インストールスクリプトを整理した
    
    Changes to be committed:
    renamed:    all_update_and_upgrade.sh -> all_update_and_upgrade_without_golang.sh
    deleted:    all_update_and_upgrade_without_rust_and_golang.sh
    
    * 誤った行をコメントアウトしていたので修正した

--- all_update_and_upgrade_without_golang.sh
+++ all_update_and_upgrade_without_golang.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-./install_and_upgrade_packages_of_golang.sh
+# ./install_and_upgrade_packages_of_golang.sh
 ./install_and_upgrade_packages_of_pip.sh
 ./install_and_upgrade_packages_of_rust.sh
 ./install_and_upgrade_packages_of_npm.sh
 
 # ./install_packages_of_rubygems.sh
 
-# ./update_rbenv_nodenv_pyenv_goenv_jenv.sh
+./update_rbenv_nodenv_pyenv_goenv_jenv.sh
 
 # 対話あり
 ./update_fzf.sh
