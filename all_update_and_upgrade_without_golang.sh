75de8da656e07b6825d21567bbffa25443407e8c
corselia <takiya@toran.sakura.ne.jp>
2020-04-19 14:43:55 +0900

    一括アップグレードのスクリプトを整理し、不要なコメント部分などを削除した

--- all_update_and_upgrade_without_golang.sh
+++ all_update_and_upgrade_without_golang.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# ./install_and_upgrade_packages_of_golang.sh
 ./install_and_upgrade_packages_of_pip.sh
 ./install_and_upgrade_packages_of_rust.sh
-./install_and_upgrade_packages_of_npm.sh
-
-# ./install_packages_of_rubygems.sh
+./update_packages_of_npm.sh
 
 ./update_rbenv_nodenv_pyenv_goenv_jenv.sh
 
-# 対話あり
 ./update_fzf.sh
-
 ./update_cli_tools.sh
 ./update_rust.sh
 
 exit 0
-
