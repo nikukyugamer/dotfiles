f91605de53d09b48818ddf7b572e1c2434edac6b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-09-20 21:37:20 +0900

    Add '--use-feature=2020-resolver' option to pip installer

--- install_and_upgrade_packages_of_pip.sh
+++ install_and_upgrade_packages_of_pip.sh
@@ -1,69 +1,99 @@
 #!/bin/bash
 
 echo 'Upgrade pip'
-pip install --upgrade pip
+pip install --upgrade --use-feature=2020-resolver pip
+echo '========================================'
 echo 'Upgrade pip Done!'
+echo '========================================'
 
 echo 'Upgrade termdown'
-pip install --upgrade termdown
+pip install --upgrade --use-feature=2020-resolver termdown
+echo '========================================'
 echo 'termdown Done!'
+echo '========================================'
 
 echo 'Upgrade setuptools'
-pip install --upgrade setuptools
+pip install --upgrade --use-feature=2020-resolver setuptools
+echo '========================================'
 echo 'Upgrade setuptools Done!'
+echo '========================================'
 
 echo 'Install and Upgrade mycli command...'
-pip install --upgrade mycli
+pip install --upgrade --use-feature=2020-resolver mycli
+echo '========================================'
 echo 'mycli Done!'
+echo '========================================'
 
 echo 'Install and Upgrade pgcli command...'
-pip install --upgrade pgcli
-pip install --upgrade psycopg2-binary
+pip install --upgrade --use-feature=2020-resolver pgcli
+pip install --upgrade --use-feature=2020-resolver psycopg2-binary
+echo '========================================'
 echo 'pgcli Done!'
+echo '========================================'
 
 echo 'Install and Upgrade google-images-download command...'
-pip install --upgrade google-images-download
+pip install --upgrade --use-feature=2020-resolver google-images-download
+echo '========================================'
 echo 'google-images-download Done!'
+echo '========================================'
 
 echo 'Install and Upgrade youtube-dl...'
-pip install --upgrade youtube-dl
+pip install --upgrade --use-feature=2020-resolver youtube-dl
+echo '========================================'
 echo 'youtube-dl Done!'
+echo '========================================'
 
 echo 'Install and Upgrade HTTPie'
-pip install --upgrade httpie
+pip install --upgrade --use-feature=2020-resolver httpie
+echo '========================================'
 echo 'HTTPie Done!'
+echo '========================================'
 
 echo 'Install and Upgrade Instaloader'
-pip install --upgrade instaloader
+pip install --upgrade --use-feature=2020-resolver instaloader
+echo '========================================'
 echo 'Instaloader Done!'
+echo '========================================'
 
 echo 'Install awscli'
-pip install --upgrade awscli
+pip install --upgrade --use-feature=2020-resolver awscli
+echo '========================================'
 echo 'awscli Done!'
+echo '========================================'
 
 echo 'Install and Upgrade awsebcli'
-pip install --upgrade awsebcli
+pip install --upgrade --use-feature=2020-resolver awsebcli
+echo '========================================'
 echo 'awsebcli Done!'
+echo '========================================'
 
 echo 'Install and Upgrade Glances (Alt top comand)'
-pip install --upgrade glances
-pip install --upgrade pysnmp
-pip install --upgrade bottle
+pip install --upgrade --use-feature=2020-resolver glances
+pip install --upgrade --use-feature=2020-resolver pysnmp
+pip install --upgrade --use-feature=2020-resolver bottle
+echo '========================================'
 echo 'glances Done!'
+echo '========================================'
 
 # https://github.com/alexdredmon/cuteuid
 echo 'Install and Upgrade awsebcli'
-pip install --upgrade cuteuid
-echo 'awsebcli Done!'
+pip install --upgrade --use-feature=2020-resolver cuteuid
+echo '========================================'
+echo 'cuteuid Done!'
+echo '========================================'
 
 # https://github.com/taspinar/twitterscraper
 echo 'Install and Upgrade twitterscraper'
-pip install --upgrade twitterscraper
+pip install --upgrade --use-feature=2020-resolver twitterscraper
+echo '========================================'
 echo 'twitterscraper Done!'
+echo '========================================'
 
 echo 'Install and Upgrade storm'
-pip install --upgrade stormssh
+pip install --upgrade --use-feature=2020-resolver stormssh
+echo '========================================'
 echo 'stormssh Done!'
+echo '========================================'
 
 exit 0
 
