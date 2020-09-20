2adbe480612a6a40f1ffb1dadc736819f1c6cf3b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-09-21 05:04:55 +0900

    Comment out some lines in the installer of RubyGems because they are no longer unnecessary

--- install_and_upgrade_packages_of_rubygems.sh
+++ install_and_upgrade_packages_of_rubygems.sh
@@ -1,27 +1,27 @@
 #!/bin/bash -xe
 
-gem install rubocop-performance
-gem install rubocop
-gem install rubocop-rails
-gem install rubocop-rspec
-gem install solargraph
-gem install rb-readline
+# gem install rubocop-performance
+# gem install rubocop
+# gem install rubocop-rails
+# gem install rubocop-rspec
+# gem install solargraph
+# gem install rb-readline
 
 # gem install mysql2
 # if MySQL version is 8, on macOS,
 # $ gem install mysql2 -- --local build.mysql2 "--with-ldflags=-L/usr/local/opt/openssl/lib"
 
-gem install pg
-gem install sqlite3
-gem install squasher
+# gem install pg
+# gem install sqlite3
+# gem install squasher
 gem install itamae
 gem install ridgepole
 gem install pry
 gem install whenever
 
 # For Visual Studio Code
-gem install ruby-debug-ide
-gem install debase
+# gem install ruby-debug-ide
+# gem install debase
 
 # https://impsbl.hatenablog.jp/entry/RubyInVSCode1
 # Solargraph さえあればどれも必須ではないっぽい（か、依存関係で入っている）
@@ -31,18 +31,18 @@ gem install debase
 # gem install faster
 # gem install debride
 
-gem install bundler-diff
+# gem install bundler-diff
 
-gem install rspec
-gem install foreman
+# gem install rspec
+# gem install foreman
 
 # For 'kv'
-gem install curses
+# gem install curses
 
-gem install twurl
+# gem install twurl
 
 # rails new 時などのトラブル防止
-gem uninstall fileutils
+# gem uninstall fileutils
 
 gem update
 
