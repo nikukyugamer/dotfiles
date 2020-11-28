7015d2c7e79fc720ffb4ad0dc4bfdf85c7d0b46c
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-11-28 12:57:00 +0900

    Add a 'phpenv' installation logic

--- system_global/install_and_update_composer.sh
+++ system_global/install_and_update_composer.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -xe
 
-mkdir composer_installer
-cd composer_installer
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-cd ..
-/bin/rm -R composer_installer
+# Deprecated: Should install Composer via phpenv's PHP
+
+# mkdir composer_installer
+# cd composer_installer
+# curl -sS https://getcomposer.org/installer | php
+# sudo mv composer.phar /usr/local/bin/composer
+# cd ..
+# /bin/rm -R composer_installer
 
 exit 0
 
