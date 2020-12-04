0f0428c25359c9e2b318cd911747ab6cc9c932df
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-12-04 10:13:25 +0900

    Add comments for node packages installation script

--- install_and_upgrade_packages_of_npm.sh
+++ install_and_upgrade_packages_of_npm.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
+echo '========================================'
 echo 'Install npm...'
 npm install -g npm
 echo 'DONE: Install npm'
+echo '========================================'
 
+echo '========================================'
 echo 'Install Yarn via npm...'
 npm install -g yarn
 echo 'DONE: Install Yarn via npm'
+echo '========================================'
 
+echo '========================================'
 echo 'Install @google/clasp...'
 npm install -g @google/clasp
 echo 'DONE: @google/clasp installation!'
+echo '========================================'
 
+echo '========================================'
 echo 'Install fx (JSON CLI tool)'
 npm install -g fx
 echo 'DONE: fx (JSON CLI tool) installation!'
+echo '========================================'
 
+echo '========================================'
 echo 'Install kmdr-cli...'
 npm install -g kmdr
 echo 'DONE: kmdr-cli installation!'
+echo '========================================'
 
+echo '========================================'
 echo 'Install Prettier...'
 npm install -g prettier
 echo 'DONE: Install Prettier'
+echo '========================================'
 
+echo '========================================'
 echo 'yukichant（テキストデータを詠唱呪文に変換するコマンド）をインストールします'
 npm install -g yukichant
 echo 'yukichant（テキストデータを詠唱呪文に変換するコマンド）のインストールが完了しました'
@@ -31,62 +44,89 @@ echo 'Usage:'
 echo '$ echo Hello,World | chant'
 echo '$ echo 巫女よ五行に元に斬る。戦慄の貪欲使命を隠し。嵐は記憶の行く手を砕ける。 | chant -d'
 echo '$ chant'
+echo '========================================'
 
+echo '========================================'
 echo 'Install pm2 (Advanced, production process manager) ...'
 npm install -g pm2
 echo 'pm2 is installed!'
+echo '========================================'
 
 # It is not necessary to install for global
 # echo 'Install textlint...'
 # npm install -g textlint
 # echo 'DONE: Install textlint'
 
+echo '========================================'
 echo 'Install jshint...'
 npm install -g jshint
 echo 'DONE: Install jshint'
+echo '========================================'
 
+echo '========================================'
 echo 'Install js-beautify...'
 npm install -g js-beautify
 echo 'DONE: Install js-beautify'
+echo '========================================'
 
+echo '========================================'
 echo 'Install typescript...'
 npm install -g typescript
 echo 'DONE: Install typescript'
+echo '========================================'
 
+echo '========================================'
 echo 'Install ts-node...'
 npm install -g ts-node
 echo 'DONE: Install ts-node'
+echo '========================================'
 
+echo '========================================'
 echo 'Install capture-website-cli...'
 npm install -g capture-website-cli
 echo 'DONE: Install capture-website-cli'
+echo '========================================'
 
+echo '========================================'
 echo 'Install @vue/cli...'
 npm install -g @vue/cli
 echo 'DONE: Install @vue/cli'
+echo '========================================'
 
+echo '========================================'
 echo 'Install @vue/cli-init...'
 npm install -g @vue/cli-init
 echo 'DONE: Install @vue/cli-init'
+echo '========================================'
 
+echo '========================================'
 echo '💪 Install @amanoese/muscular... 💪'
 npm install -g @amanoese/muscular
 echo '💪 DONE: Install @vue/cli-init 💪'
+echo '========================================'
 
+echo '========================================'
 echo 'Install "trello-cli"'
 npm install -g trello-cli
 echo 'DONE: Install "trello-cli"'
+echo '========================================'
 
+echo '========================================'
 echo 'Install "jsonlint"'
 npm install -g jsonlint
 echo 'DONE: Install "jsonlint"'
+echo '========================================'
 
+echo '========================================'
 echo 'Install @fand/kao'
 npm install -g @fand/kao
 echo 'DONE: Install @fand/kao'
+echo '========================================'
 
+echo '========================================'
 echo 'Install dsstore-cli (Remove .DS_Store)'
 npm install -g dsstore-cli
 echo 'DONE: Install dsstore-cli'
+echo '========================================'
 
 exit 0
