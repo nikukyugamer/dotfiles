e033a8c7a9a238f02860ed1a090d3ea1aedc1e86
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-12-28 00:18:54 +0900

    feat(.czrc): Add a configuration file for Commitizen

--- copy_config_files_and_eachenv.sh
+++ copy_config_files_and_eachenv.sh
@@ -3,5 +3,6 @@
 cp .zshrc.eachenv.should_cp_to_home_directory ~/.zshrc
 cp .vimrc.should_cp_to_home_directory ~/.vimrc
 cp .gitconfig.should_cp_to_home_directory ~/.gitconfig
+cp .czrc ~/.czrc
 
 exit 0
