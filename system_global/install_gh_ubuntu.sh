4b0ecce1506969f3dd02d99b2a3dc4758a197f8f
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-01-03 06:53:28 +0900

    Add a 'gh' instllation script for Ubuntu

---
+++ system_global/install_gh_ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update -y
+sudo apt install -y gh
+
+exit
+
