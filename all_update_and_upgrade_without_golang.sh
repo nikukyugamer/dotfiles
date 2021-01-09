8fda405b2f30c6e5049817f078eed9f0ba262f83
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-01-09 14:25:05 +0900

    fix: 🐛 All update script for "php-build"

--- all_update_and_upgrade_without_golang.sh
+++ all_update_and_upgrade_without_golang.sh
@@ -4,7 +4,7 @@
 ./install_and_upgrade_packages_of_rust.sh
 ./update_packages_of_npm.sh
 
-./update_rbenv_nodenv_pyenv_goenv_jenv.sh
+./update_rbenv_nodenv_pyenv_goenv_jenv_phpenv.sh
 
 ./update_fzf.sh
 ./update_cli_tools.sh
