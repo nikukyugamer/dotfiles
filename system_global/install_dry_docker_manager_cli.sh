47be248cec4522e67cd3338c8fc6a8ca146e8565
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-01-23 21:56:41 +0900

    feat: 🎸 Add an installtion script for "dry" (Docker CLI manage)

---
+++ system_global/install_dry_docker_manager_cli.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -xe
+
+# Ubuntu
+# wget https://github.com/moncho/dry/releases/download/v0.10-beta.1/dry-linux-amd64
+# chmod +x dry-linux-amd64
+# mv dry-linux-amd64 dry
+# sudo mv dry /usr/local/bin
+
+# Raspberry Pi
+# wget https://github.com/moncho/dry/releases/download/v0.10-beta.1/dry-linux-arm64
+# chmod +x dry-linux-arm64
+# mv dry-linux-arm64 dry
+# sudo mv dry /usr/local/bin
+
+# Darwin
+# wget https://github.com/moncho/dry/releases/download/v0.10-beta.1/dry-darwin-amd64
+# chmod +x dry-darwin-amd64
+# mv dry-darwin-amd64 dry
+# sudo mv dry /usr/local/bin
+
+exit 0
+
