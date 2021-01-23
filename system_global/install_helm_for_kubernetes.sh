2975c2d3f55c9f3c671832ed272084f7416ca274
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-01-23 21:55:57 +0900

    feat: 🎸 Add an installation script for "helm" (for Kubernetes)

---
+++ system_global/install_helm_for_kubernetes.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -xe
+
+# https://helm.sh/docs/intro/install/
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt update -y
+sudo apt install helm -y
+
+exit 0
+
