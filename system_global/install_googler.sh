7e5c9385ac3f312cdc2ed73063a6ebf2cfd6570d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-02-07 06:01:19 +0900

    feat: 🎸 Google のインストールスクリプトを追加した

---
+++ system_global/install_googler.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -xe
+
+# https://github.com/jarun/googler/releases
+# wget https://github.com/jarun/googler/releases/download/v4.3.2/googler_4.3.2-1_ubuntu20.04.amd64.deb
+# sudo dpkg -i googler_4.3.2-1_ubuntu20.04.amd64.deb
+# rm googler_4.3.2-1_ubuntu20.04.amd64.deb
+
+exit 0
+
