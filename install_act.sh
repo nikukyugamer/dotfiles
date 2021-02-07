3c0b1555a80d6afa179f8b7e92bba977a9519293
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-02-07 19:16:13 +0900

    feat: 🎸 Add an "act" installation script (GitHub Actions CLI)

---
+++ install_act.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xe
+
+# https://github.com/nektos/act
+# Run your GitHub Actions locally! Why would you want to do this? Two reasons:
+curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+
+exit 0
+
