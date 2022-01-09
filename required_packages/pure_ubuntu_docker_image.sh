46e18b5a64d31d0db7697833e3989cfce53d3c07
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-09 16:46:50 +0900

    feat: 🎸 required_packages のスクリプトを追加（Firefoxなど）

--- required_packages/pure_ubuntu_docker_image.sh
+++ required_packages/pure_ubuntu_docker_image.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-sudo apt install -y wget curl build-essential git peco language-pack-ja software-properties-common
+sudo apt update
+sudo apt dist-upgrade -y
+sudo apt install -y --force-yes --no-install-recommends wget curl build-essential git peco language-pack-ja software-properties-common
+
+sudo apt clean
+sudo apt autoremove
 
 exit 0
