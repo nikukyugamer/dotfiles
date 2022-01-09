37b1d01788868a2cf658fc1b9b8b3c93d5a1c382
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-09 13:55:07 +0900

    feat: 🎸 Go のインストールスクリプトをリファクタリングした

--- install_and_upgrade_packages_of_golang.sh
+++ install_and_upgrade_packages_of_golang.sh
@@ -1,229 +1,67 @@
 #!/bin/bash
 
-echo '========================================'
-echo 'Init directories for Go...'
-mkdir -p ~/.go
-mkdir -p ~/.ghq
-echo '========================================'
+function go_install() {
+  echo '======================================================='
+  echo "[LOG] START: $ go install $1"
+  echo '======================================================='
 
-echo '========================================'
-echo 'Install/Update Docker Hub Tool "hub-tool"...'
-GO111MODULE=on go install github.com/docker/hub-tool@latest
-echo 'hub-tool installation is done.'
-echo '========================================'
+  go install "$1"
 
-echo '========================================'
-echo 'Install/Update act (GitHub Actions Local Exec Tool)'
-go install github.com/nektos/act@latest
-echo 'An act installation is done.'
-echo '========================================'
+  echo '======================================================='
+  echo "[LOG] END: $ go install $1"
+  echo '======================================================='
 
-echo '========================================'
-echo 'Install/Update IPinfo (https://ipinfo.io/)'
-go install github.com/ipinfo/cli/ipinfo@latest
-echo 'An IPinfo installation is done.'
-echo '========================================'
+  echo '**********************************************************************'
+}
 
-echo '========================================'
-echo 'Install/Update A time zone helper'
-go install github.com/oz/tz@latest
-echo 'A time zone helper installation is done.'
-echo '========================================'
+function init_directories() {
+  echo '======================================================='
+  echo "[LOG] START: Init directories for Go"
+  echo '======================================================='
 
-echo '========================================'
-echo 'Install/Update A tool for exploring a docker image'
-go install github.com/wagoodman/dive@latest
-echo 'A tool for exploring a docker image installation is done.'
-echo '========================================'
+  mkdir -p ~/.go
+  mkdir -p ~/.ghq
 
-echo '========================================'
-echo 'Install/Update GoTTY (a simple command line tool that turns your CLI tools into web applications)'
-go install github.com/yudai/gotty@latest
-echo 'GoTTY installation is done.'
-echo '========================================'
+  echo '======================================================='
+  echo "[LOG] END: Init directories for Go"
+  echo '======================================================='
 
-echo '========================================'
-echo 'Install/Update Goreman (A Clone of foreman written in golang)'
-go install github.com/mattn/goreman@latest
-echo 'A Goreman installation is done.'
-echo '========================================'
+  echo '**********************************************************************'
+}
 
-echo '========================================'
-echo 'Install/Update Vegeta (A versatile HTTP load testing tool)'
-go install github.com/tsenart/vegeta@latest
-echo 'Vegeta Done.'
-echo 'cf. https://camo.githubusercontent.com/417a39e5a142e0877be0a7a6d7a66cb77ea21e8c/687474703a2f2f666330392e64657669616e746172742e6e65742f667334392f692f323030392f3139382f632f632f73736a325f7665676574615f62795f7472756e6b7332342e6a7067'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update fillin'
-echo 'https://github.com/itchyny/fillin'
-go install github.com/itchyny/fillin@latest
-echo '"fillin" installation has been done.'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update lazynpm'
-go install github.com/jesseduffield/lazynpm@latest
-echo 'lazynpm installation has been done.'
-echo '========================================'
-
-# https://github.com/jesseduffield/lazygit
-go install github.com/jesseduffield/lazygit@latest
-
-echo '========================================'
-echo 'Install/Update longcat'
-go install github.com/mattn/longcat@latest
-echo 'Looooooooooooooooooooooooooooooooooooooooooooooong cat Done.'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update ghch (Generate changelog from git history)'
-go install github.com/Songmu/ghch/cmd/ghch@latest
-echo 'ghch Done.'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update ojichatrix'
-go install github.com/greymd/ojichatrix@latest
-echo 'ojichatrix Done.'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update Tokyo Amesh...'
-go install github.com/otiai10/amesh@latest
-echo 'Tokyo Amesh Done.'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update ghq...'
-go install github.com/x-motemen/ghq@latest
-echo 'ghq Done'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update hub...'
-go install github.com/github/hub@latest
-echo 'hub Done'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update gomi...'
-go install github.com/b4b4r07/gomi@latest
-echo 'gomi Done'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update pp...'
-# or github.com/davecgh/go-spew/spew@latest
-go install github.com/k0kubun/pp@latest
-echo 'pp Done'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update dlv...'
-# for VSCode
-go install github.com/go-delve/delve/cmd/dlv@latest
-echo 'dlv Done'
-echo '========================================'
-
-# Duplicated? -> (github.com/mdempsky/gocode)
-echo '========================================'
-echo 'Install/Update stamblerre/gocode...'
-# for VSCode
-go install github.com/stamblerre/gocode@latest
-echo 'gocode Done'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update golint...'
-# for VSCode
-go install golang.org/x/lint/golint@latest
-echo 'golint Done'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update jpcal...'
-go install github.com/y-yagi/jpcal@latest
-echo 'jpcal Done'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update ojichat...'
-go install github.com/greymd/ojichat@latest
-echo 'ojichat Done'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update lazydocker...'
-go install github.com/jesseduffield/lazydocker@latest
-echo 'lazydocker Done'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update Slack API in Go...'
-go install github.com/nlopes/slack@latest
-echo 'Slack API in Go Done'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update "memo" by mattn...'
-go install github.com/mattn/memo@latest
-echo '"memo" Done'
-echo '========================================'
-
-# https://github.com/qeesung/asciiplayer
-echo '========================================'
-echo 'Install/Update "ASCIIPlayer"...'
-go install github.com/qeesung/asciiplayer@latest
-echo '"ASCIIPlayer" Done'
-echo '========================================'
-
-# Twitter Clinet Powered by mattn
-echo '========================================'
-echo 'Install/Update twty (Twitter Client) developed by mattn'
-go install github.com/mattn/twty@latest
-echo 'twty (Twitter Client) Done.'
-echo '========================================'
-
-# Rename multiple files using your $EDITOR
-# https://github.com/itchyny/mmv
-echo '========================================'
-echo 'Install/Update mmv'
-go install github.com/itchyny/mmv/cmd/mmv@latest
-echo 'mmv Done.'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update jsonui'
-go install github.com/gulyasm/jsonui@latest
-echo 'jsonui Done.'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update bed (Binary editor written in Go)'
-go install github.com/itchyny/bed/cmd/bed@latest
-echo 'bed Done.'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update Gin (Web Framework)'
-go install github.com/gin-gonic/gin@latest
-echo 'Gin installation has been done.'
-echo '========================================'
-
-echo '========================================'
-echo 'Install/Update echo (Web Framework)'
-go install github.com/labstack/echo@latest
-echo 'echo installation has been done.'
-echo '========================================'
-
-# https://github.com/golang/tools/tree/master/gopls
-echo '========================================'
-echo 'Install/Update gopls, the Go language server'
-GO111MODULE=on go install golang.org/x/tools/gopls@latest
-echo 'gopls installation has been done.'
-echo '========================================'
+go_install github.com/docker/hub-tool@latest # GO111MODULE=on
+go_install github.com/nektos/act@latest # act (GitHub Actions Local Exec Tool)
+go_install github.com/ipinfo/cli/ipinfo@latest # IPinfo (https://ipinfo.io/)
+go_install github.com/oz/tz@latest # A time zone helper
+# go_install github.com/wagoodman/dive@latest # A tool for exploring a docker image
+# go_install github.com/yudai/gotty@latest # a simple command line tool that turns your CLI tools into web applications
+go_install github.com/mattn/goreman@latest # A Clone of foreman written in golang
+# go_install github.com/tsenart/vegeta@latest # A versatile HTTP load testing tool
+go_install github.com/itchyny/fillin@latest
+go_install github.com/jesseduffield/lazynpm@latest
+go_install github.com/jesseduffield/lazygit@latest # https://github.com/jesseduffield/lazygit
+go_install github.com/mattn/longcat@latest
+# go_install github.com/Songmu/ghch/cmd/ghch@latest # Generate changelog from git history
+go_install github.com/greymd/ojichatrix@latest
+go_install github.com/otiai10/amesh@latest
+go_install github.com/x-motemen/ghq@latest
+# go_install github.com/github/hub@latest
+go_install github.com/b4b4r07/gomi@latest
+# go_install github.com/k0kubun/pp@latest # or github.com/davecgh/go-spew/spew@latest
+go_install github.com/go-delve/delve/cmd/dlv@latest # for VSCode
+# go_install github.com/stamblerre/gocode@latest# # for VSCode. Duplicated? -> (github.com/mdempsky/gocode)
+go_install golang.org/x/lint/golint@latest # for VSCode
+go_install github.com/y-yagi/jpcal@latest
+go_install github.com/greymd/ojichat@latest
+go_install github.com/jesseduffield/lazydocker@latest
+go_install github.com/nlopes/slack@latest
+go_install github.com/mattn/memo@latest
+# go_install github.com/qeesung/asciiplayer@latest # https://github.com/qeesung/asciiplayer
+go_install github.com/mattn/twty@latest # twty (Twitter Client) developed by mattn
+go_install github.com/itchyny/mmv/cmd/mmv@latest # Rename multiple files using your $EDITOR https://github.com/itchyny/mmv
+# go_install github.com/gulyasm/jsonui@latest
+go_install github.com/itchyny/bed/cmd/bed@latest # Binary editor written in Go
+go_install github.com/gin-gonic/gin@latest # Gin (Web Framework)
+go_install github.com/labstack/echo/v4@latest # echo (Web Framework) https://echo.labstack.com/
 
 exit 0
-
