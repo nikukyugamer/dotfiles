2f8d7053beb57f6022429eb5ade670d18fc07b93
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-09 16:23:33 +0900

    feat: 🎸 ミドルウェアなどごとに必要な OS パッケージを整理してインストールスクリプトを作成

---
+++ required_packages/cypress.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# https://docs.cypress.io/guides/continuous-integration/introduction#Dependencies
+sudo apt install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+
+exit 0
