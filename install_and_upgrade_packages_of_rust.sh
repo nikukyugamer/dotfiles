5364a61f8fa106e2dbbfad5e86bf80f12314b65b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-09 12:36:00 +0900

    feat: 🎸 cargo のインストールスクリプトをリファクタリング

--- install_and_upgrade_packages_of_rust.sh
+++ install_and_upgrade_packages_of_rust.sh
@@ -1,170 +1,56 @@
 #!/bin/bash
 
-echo '========================================'
-echo 'Install cargo-update'
-cargo install cargo-update
-echo 'A "cargo-update" installation has done.'
-echo '========================================'
+function install_cargo() {
+  echo '======================================================='
+  echo "[LOG] START: $ cargo install $1"
+  echo '======================================================='
 
-cargo install-update --all
+  cargo install "$1"
 
-echo '========================================'
-echo 'Install Deno'
-echo 'Deno is a JavaScript/TypeScript runtime with secure defaults and a great developer experience.'
-# cargo install deno --locked
-# thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: Unable to build http client: builder error: Unknown TLS backend passed to `use_preconfigured_tls`'
-echo '"Deno" installation has done'
-echo '========================================'
+  echo '======================================================='
+  echo "[LOG] END: $ cargo install $1"
+  echo '======================================================='
+}
 
-cargo install git-interactive-rebase-tool
-cargo install onefetch
+function cargo_install_update_all() {
+  echo '======================================================='
+  echo "[LOG] START: $ cargo install-update --all"
+  echo '======================================================='
 
-echo '========================================'
-echo 'GitUI provides you with the comfort of a git GUI but right in your terminal'
-echo 'Install gitui'
-# https://github.com/extrawurst/gitui
-cargo install gitui
-echo '"gitui" installation has done'
-echo '========================================'
+  cargo install-update --all
 
-echo '========================================'
-echo 'Install tokei'
-# https://github.com/XAMPPRocky/tokei
-cargo install tokei
-echo '"tokei" installation has done'
-echo '========================================'
+  echo '======================================================='
+  echo "[LOG] END: $ cargo install-update --all"
+  echo '======================================================='
+}
 
-echo '========================================'
-echo 'Install ripgrep-all'
-# https://github.com/phiresky/ripgrep-all
-cargo install ripgrep_all
-echo '"ripgrep-all" installation has done'
-echo '========================================'
+install_cargo cargo-update
+cargo_install_update_all
 
-echo '========================================'
-echo 'install miniserve'
-echo 'miniserve - a CLI tool to serve files and dirs over HTTP'
-# https://github.com/svenstaro/miniserve/issues/568
-cargo install miniserve --locked
-echo '"miniserver" installation has done'
-echo '========================================'
-
-echo '========================================'
-# https://github.com/Peltoche/lsd
-echo 'Install "lsd"（モダンな "ls" コマンド）'
-cargo install --git https://github.com/Peltoche/lsd.git --branch master
-echo '"lsd" installation has done'
-echo '========================================'
-
-echo '========================================'
-echo 'install simple-http-server'
-cargo install simple-http-server
-echo '"simple-http-server" installation has done'
-echo '========================================'
-
-echo '========================================'
-echo "Install diskonaut (getting a visual treemap representation of what's taking up your disk space)"
-cargo install diskonaut
-echo '"diskonaut" installation has done'
-echo '========================================'
-
-echo '========================================'
-echo 'Install broot (A new way to see and navigate directory trees)'
-cargo install broot
-echo '"broot" installation has done'
-echo '========================================'
-
-echo '========================================'
-echo 'Install delta (git-delta) (A syntax-highlighter for git and diff output)'
-cargo install git-delta
-echo '"delta (git-delta)" installation has done'
-echo '========================================'
-
-echo '========================================'
-echo 'Install tealdeer (Community based "tlde" command, which is the same as "man" command)'
-cargo install tealdeer
+# install_cargo deno --locked
+install_cargo git-interactive-rebase-tool
+install_cargo onefetch
+install_cargo gitui # https://github.com/extrawurst/gitui
+install_cargo tokei # https://github.com/XAMPPRocky/tokei
+install_cargo ripgrep_all # https://github.com/phiresky/ripgrep-all
+install_cargo miniserve --locked # https://github.com/svenstaro/miniserve/issues/568
+cargo install --git https://github.com/Peltoche/lsd.git --branch master # https://github.com/Peltoche/lsd
+install_cargo simple-http-server
+install_cargo diskonaut
+install_cargo broot
+install_cargo git-delta
+install_cargo tealdeer
 tldr --update
-echo '"tealdeer" installation is done.'
-echo '========================================'
-
-echo '========================================'
-echo 'Install dust (du command powered by Rust)'
-cargo install du-dust
-echo 'dust installation is done.'
-echo '========================================'
-
-echo '========================================'
-echo 'Install procs (Alt ps command)'
-cargo install procs
-echo 'procs installation is done.'
-echo '========================================'
-
-echo '========================================'
-echo 'Install grex...'
-cargo install grex
-echo 'grex installation is done.'
-echo 'grex is a small command-line utility that is meant to simplify the often complicated and tedious task of creating regular expressions.'
-echo '========================================'
-
-echo '========================================'
-echo 'Install bat...'
-cargo install bat
-echo 'bat Done.'
-echo 'If you would like to show without line numbers, use "-p" option'
-echo '========================================'
-
-echo '========================================'
-echo 'Install ripgrep...'
-cargo install ripgrep
-echo 'ripgrep Done.'
-echo 'Usage: $ rga "hello" demo/'
-echo '========================================'
-
-echo '========================================'
-echo 'Install fd(fd-find)...'
-cargo install fd-find
-echo 'fd(fd-find) Done.'
-echo 'Usage: $ fd "^x.*rc$"'
-echo '========================================'
-
-echo '========================================'
-echo 'Install exa...'
-cargo install exa
-echo 'exa Done.'
-echo 'Usage: $ exa -Tl'
-echo '========================================'
-
-echo '========================================'
-echo 'Install Silicon (Capture source codes)...'
-cargo install silicon
-echo 'Silicon Done.'
-echo 'Usage: $ silicon main.rs -o main.png'
-echo '========================================'
-
-echo '========================================'
-echo 'Install kiro-editor (Like micro)...'
-cargo install kiro-editor
-echo 'kill-editor installation has been done.'
-echo 'Usage: $ kiro hoge.txt'
-echo '========================================'
-
-echo '========================================'
-echo 'Install monolith (Pack a webpage to a single file)'
-# git clone https://github.com/Y2Z/monolith.git /tmp/monolith
-# cd /tmp/monolith
-# ~/.cargo/bin/cargo install --force --path .
-# cd ~ && rm -rf /tmp/monolith
-cargo install monolith
-echo '"monolith" installation has done.'
-echo 'Usage: $ monolith https://www.amazon.co.jp/ > amazon.html'
-echo '========================================'
-
-echo '========================================'
-echo 'Install "angle-grinder" (Logs analytics CLI tool)'
-cargo install ag
-echo '"angle-grinder" installation has done.'
-echo 'Usage: $ agrind --help'
-echo '========================================'
+install_cargo du-dust
+install_cargo procs
+install_cargo grex
+install_cargo bat
+install_cargo ripgrep
+install_cargo fd-find
+install_cargo exa
+install_cargo silicon
+install_cargo kiro-editor
+install_cargo monolith # https://github.com/Y2Z/monolith
+install_cargo ag
 
 exit 0
-
