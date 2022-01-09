c1d48e05d533cfffd57039165b308416c0fa126b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-09 14:06:28 +0900

    refactor: 💡 cargo のインストール順をアルファベット順に変更

--- install_and_upgrade_packages_of_rust.sh
+++ install_and_upgrade_packages_of_rust.sh
@@ -29,30 +29,32 @@ function cargo_install_update_all() {
 install_cargo cargo-update
 cargo_install_update_all
 
-# install_cargo deno --locked
-install_cargo git-interactive-rebase-tool
-install_cargo onefetch
-install_cargo gitui # https://github.com/extrawurst/gitui
-install_cargo tokei # https://github.com/XAMPPRocky/tokei
-install_cargo ripgrep_all # https://github.com/phiresky/ripgrep-all
-install_cargo miniserve --locked # https://github.com/svenstaro/miniserve/issues/568
 cargo install --git https://github.com/Peltoche/lsd.git --branch master # https://github.com/Peltoche/lsd
-install_cargo simple-http-server
-install_cargo diskonaut
-install_cargo broot
-install_cargo git-delta
-install_cargo tealdeer
-tldr --update
-install_cargo du-dust
-install_cargo procs
-install_cargo grex
-install_cargo bat
-install_cargo ripgrep
-install_cargo fd-find
-install_cargo exa
-install_cargo silicon
-install_cargo kiro-editor
-install_cargo monolith # https://github.com/Y2Z/monolith
 install_cargo ag
+install_cargo bat
+install_cargo broot
+install_cargo diskonaut
+install_cargo du-dust
+install_cargo exa
+install_cargo fd-find
+install_cargo git-delta
+install_cargo git-interactive-rebase-tool
+install_cargo gitui # https://github.com/extrawurst/gitui
+install_cargo grex
+install_cargo kiro-editor
+install_cargo miniserve --locked # https://github.com/svenstaro/miniserve/issues/568
+install_cargo monolith # https://github.com/Y2Z/monolith
+install_cargo onefetch
+install_cargo procs
+install_cargo ripgrep
+install_cargo ripgrep_all # https://github.com/phiresky/ripgrep-all
+install_cargo silicon
+install_cargo simple-http-server
+install_cargo tealdeer
+install_cargo tokei # https://github.com/XAMPPRocky/tokei
+
+tldr --update
+
+# install_cargo deno --locked
 
 exit 0
