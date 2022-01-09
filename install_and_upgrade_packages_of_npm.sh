c0d21b599a263802bb50438ba8c046d1ab88e631
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-09 14:05:37 +0900

    refactor: 💡 npm のインストール順をアルファベット順に変更

--- install_and_upgrade_packages_of_npm.sh
+++ install_and_upgrade_packages_of_npm.sh
@@ -41,28 +41,29 @@ function install_git_cz() {
   echo '**********************************************************************'
 }
 
-npm_install_g yarn
-npm_install_g yarn-outdated-formatter # https://github.com/masawada/yarn-outdated-formatter
-npm_install_g npm-check-updates
-npm_install_g zx
+npm_install_g @amanoese/muscular
+npm_install_g @fand/kao
 npm_install_g @google/clasp
-npm_install_g fx
-npm_install_g kmdr
-npm_install_g yukichant
-npm_install_g pm2
-npm_install_g jshint
-npm_install_g js-beautify
-npm_install_g typescript
-npm_install_g ts-node
-npm_install_g capture-website-cli
 npm_install_g @vue/cli
 npm_install_g @vue/cli-init
-npm_install_g @amanoese/muscular
-npm_install_g trello-cli
-npm_install_g jsonlint
-npm_install_g @fand/kao
-npm_install_g dsstore-cli
+npm_install_g capture-website-cli
 npm_install_g diff-so-fancy
+npm_install_g dsstore-cli
+npm_install_g fx
+npm_install_g js-beautify
+npm_install_g jshint
+npm_install_g jsonlint
+npm_install_g kmdr
+npm_install_g npm-check-updates
+npm_install_g pm2
+npm_install_g trello-cli
+npm_install_g ts-node
+npm_install_g typescript
+npm_install_g yarn
+npm_install_g yarn-outdated-formatter # https://github.com/masawada/yarn-outdated-formatter
+npm_install_g yukichant
+npm_install_g zx
+
 install_git_cz
 
 exit 0
