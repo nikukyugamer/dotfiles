1a3155a0584c556b6f428a4eada95f03f9d53413
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-09 14:04:58 +0900

    refactor: 💡 pip のインストール順をアルファベット順に変更

--- install_and_upgrade_packages_of_pip.sh
+++ install_and_upgrade_packages_of_pip.sh
@@ -17,29 +17,30 @@ function install_pip() {
 install_pip pip
 install_pip pipx # https://github.com/pypa/pipx
 install_pip pipenv # https://github.com/pypa/pipenv
-install_pip sen # https://github.com/TomasTomecek/sen
+
 install_pip aws-encryption-sdk-cli # https://aws-encryption-sdk-cli.readthedocs.io/en/latest/
-install_pip pyxattr
-install_pip yt-dlp
-install_pip https://github.com/egbertbouman/youtube-comment-downloader/archive/master.zip
+install_pip awsebcli
+install_pip bottle
 install_pip chat_downloader
-install_pip wad # WAD (Web application detector)
-install_pip ranger-fm # ranger (CLI Filer)
-install_pip termdown
-install_pip setuptools
+install_pip cuteuid # https://github.com/alexdredmon/cuteuid
+install_pip glances
+install_pip httpie
+install_pip https://github.com/egbertbouman/youtube-comment-downloader/archive/master.zip
+install_pip instaloader
 install_pip mycli
 install_pip pgcli
 install_pip psycopg2-binary
-install_pip youtube-dl
-install_pip httpie
-install_pip instaloader
-install_pip awsebcli
-install_pip glances
 install_pip pysnmp
-install_pip bottle
-install_pip cuteuid # https://github.com/alexdredmon/cuteuid
-install_pip twitterscraper # https://github.com/taspinar/twitterscraper
+install_pip pyxattr
+install_pip ranger-fm # ranger (CLI Filer)
+install_pip sen # https://github.com/TomasTomecek/sen
+install_pip setuptools
 install_pip stormssh
 install_pip streamlink
+install_pip termdown
+install_pip twitterscraper # https://github.com/taspinar/twitterscraper
+install_pip wad # WAD (Web application detector)
+install_pip youtube-dl
+install_pip yt-dlp
 
 exit 0
