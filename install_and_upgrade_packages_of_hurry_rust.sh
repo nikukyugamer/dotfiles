7a7f756a34606883b9ca49ce5376a2451a093df8
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-09 14:54:11 +0900

    feat: 🎸 cargo のミニマムインストール版を追加した

---
+++ install_and_upgrade_packages_of_hurry_rust.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ミニマムインストール用
+
+function install_cargo() {
+  echo '======================================================='
+  echo "[LOG] START: $ cargo install $1"
+  echo '======================================================='
+
+  cargo install "$1"
+
+  echo '======================================================='
+  echo "[LOG] END: $ cargo install $1"
+  echo '======================================================='
+}
+
+cargo install --git https://github.com/Peltoche/lsd.git --branch master # https://github.com/Peltoche/lsd
+install_cargo bat
+install_cargo exa
+
+exit 0
