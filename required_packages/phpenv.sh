88ee487fae7b7a870bc72e40b34df04d339c2047
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-09 16:24:27 +0900

    feat: 🎸 phpenv に必要なインストールパッケージを追加

---
+++ required_packages/phpenv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install -y bison re2c libxml2-dev libssl-dev libbz2-dev libcurl4-openssl-dev libjpeg-dev libpng-dev libreadline-dev libtidy-dev libxslt1-dev libzip-dev autoconf
+
+exit 0
