6da8ed2769e9915ed7b4c4e2a4abf9eabcfb9f89
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-11 10:11:12 +0900

    feat: 🎸 "gosu" のインストールスクリプトを追加した

---
+++ system_global/install_gosu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+# amd64
+wget https://github.com/tianon/gosu/releases/download/1.14/gosu-amd64 -O /tmp/gosu
+chmod +x /tmp/gosu
+sudo mv /tmp/gosu /usr/local/bin
+
+echo "---------------------------------------------------------------------------"
+echo "[LOG] $ which gosu"
+which gosu
+echo "---------------------------------------------------------------------------"
+echo "[LOG] $ gosu version"
+gosu version
+
+exit 0
