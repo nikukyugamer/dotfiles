d57cb87e126c4e798af99faa15e8da787b0c1738
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-02-11 23:23:57 +0900

    feat: 🎸 jenv で Java を入れるスクリプトを追加した

---
+++ install_java.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install -y openjdk-8-jdk
+sudo apt install -y openjdk-11-jdk
+
+jenv add /usr/lib/jvm/java-8-openjdk-amd64/
+jenv add /usr/lib/jvm/java-11-openjdk-amd64/
+
+jenv versions
+
+exit 0
