180730f46539504f4e5b8fc3e1b3769923a69a72
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-02-14 08:31:14 +0900

    feat: 🎸 Silicon 用の requried packages を更新した

--- required_packages/rust_apps.sh
+++ required_packages/rust_apps.sh
@@ -8,4 +8,10 @@ sudo apt install -y build-essential clang cmake git libssl-dev pkg-config
 # https://github.com/phiresky/ripgrep-all#debian-based
 # sudo apt install -y ripgrep pandoc poppler-utils ffmpeg
 
+# Silicon
+# https://github.com/Aloxaf/silicon
+sudo apt install -y expat
+sudo apt install -y libxml2-dev
+sudo apt install -y pkg-config libasound2-dev libssl-dev cmake libfreetype6-dev libexpat1-dev libxcb-composite0-dev
+
 exit 0
