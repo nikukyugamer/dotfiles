69e1064bb471a6b0b2e06f0547d34827ff24be3a
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-02-19 00:00:26 +0900

    feat: 🎸 dog の go実装 である doggo のインストールスクリプトを追加した

--- install_and_upgrade_packages_of_golang.sh
+++ install_and_upgrade_packages_of_golang.sh
@@ -54,4 +54,7 @@ go install -v golang.org/x/tools/gopls@latest
 go install -v github.com/ramya-rao-a/go-outline@latest
 go install -v golang.org/x/tools/cmd/goimports@latest
 
+# dog on go = doggo
+go install -v github.com/mr-karan/doggo/cmd/doggo@latest
+
 exit 0
