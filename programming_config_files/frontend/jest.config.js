ad93d965f2f498f12364834cabbf2ba98976ad6b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-03-21 15:56:06 +0900

    feat: 🎸 フロントエンドの設定ファイルを更新

--- programming_config_files/frontend/jest.config.js
+++ programming_config_files/frontend/jest.config.js
@@ -1,7 +1,6 @@
-// $ yarn add --dev babel-jest も必要に応じて行う
 module.exports = {
   verbose: true,
   testPathIgnorePatterns: ['/node_modules/', '/.history/'],
   testTimeout: 30000,
   preset: 'jest-puppeteer',
-};
+}
