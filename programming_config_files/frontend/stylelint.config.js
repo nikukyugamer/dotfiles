a291d4c2ba5453d74e7a56a8976a412165270d60
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-04-11 19:51:06 +0900

    test: 💍 Stylelint の設定ファイルを更新

--- programming_config_files/frontend/stylelint.config.js
+++ programming_config_files/frontend/stylelint.config.js
@@ -1,4 +1,16 @@
 module.exports = {
-  extends: 'stylelint-config-standard',
+  "plugins": ["stylelint-order"],
+  "extends": ["stylelint-config-standard"],
+  "rules": {
+    "at-rule-no-unknown": [
+      true,
+      {
+        "ignoreAtRules": ["tailwind", "apply", "variants", "responsive", "screen"]
+      }
+    ],
+    "string-quotes": "single",
+    "order/properties-alphabetical-order": true
+  },
+  "ignoreFiles": ["**/node_modules/**"]
 }
 
