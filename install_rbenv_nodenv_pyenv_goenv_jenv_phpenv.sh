055323ef906bab17f7b34117c3e8bcf42f92eae4
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-04-23 21:35:02 +0900

    feat: 🎸 phpenv の clone を HTTPS にした（SSH は非推奨のため）

--- install_rbenv_nodenv_pyenv_goenv_jenv_phpenv.sh
+++ install_rbenv_nodenv_pyenv_goenv_jenv_phpenv.sh
@@ -20,7 +20,7 @@ git clone https://github.com/jenv/jenv.git ~/.jenv
 
 # https://github.com/phpenv/phpenv
 # Required: bzip2
-git clone git://github.com/phpenv/phpenv.git ~/.phpenv
+git clone https://github.com/phpenv/phpenv.git ~/.phpenv
 git clone https://github.com/php-build/php-build ~/.phpenv/plugins/php-build
 git clone https://github.com/ngyuki/phpenv-composer.git ~/.phpenv/plugins/phpenv-composer
 
