3ae4461b978dea488fba2a56fcc66843a3c856fb
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-05-06 09:40:48 +0900

    feat: 🎸 peco はパッケージマネージャで入れるのでインストールスクリプトは削除した

--- system_global/install_peco_for_arm32.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/peco/peco/releases/download/v0.5.3/peco_linux_arm.tar.gz
-tar -zxvf peco_linux_arm.tar.gz
-sudo cp peco_linux_arm/peco /usr/local/bin/peco
-rm -r -f peco_linux_arm
-rm peco_linux_arm.tar.gz
