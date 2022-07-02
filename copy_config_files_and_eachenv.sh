760230c73ad26bde74d93acc8421efe4635b43a9
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-07-02 15:36:42 +0900

    feat: 🎸 上書きを避けるために .gitconfig のファイル名を変更した

--- copy_config_files_and_eachenv.sh
+++ copy_config_files_and_eachenv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -xe
 
 cp .zshrc.eachenv.should_cp_to_home_directory.zshrc ~/.zshrc
-cp .zshrc.userown.zshrc ~/.zshrc.userown.zshrc
+cp .zshrc.userown.zshrc ~/.zshrc.userown.zshrc.sample
 cp .vimrc.should_cp_to_home_directory ~/.vimrc
-cp .gitconfig.should_cp_to_home_directory ~/.gitconfig
+cp .gitconfig.should_cp_to_home_directory ~/.gitconfig.sample
 # cp .czrc ~/.czrc
 
 exit 0
