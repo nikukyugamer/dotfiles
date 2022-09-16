c90d96b0582faaa5bc6fec113a8c6e1f1cb5041d
nikukyugamer <takiya@toran.sakura.ne.jp>
2022-09-16 09:18:41 +0900

    feat: 🎸 pyenv に必要なパッケージインストーラを最新のものにアップデートした

--- system_global/install_os_packages/install_for_pyenv_build_lib_ubuntu.sh
+++ system_global/install_os_packages/install_for_pyenv_build_lib_ubuntu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
 
-sudo apt-get install libbz2-dev libreadline-dev libsqlite3-dev libffi-dev -y
+# https://github.com/pyenv/pyenv/wiki
+sudo apt-get update; sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 # For Rust?
 sudo apt-get install clang -y
