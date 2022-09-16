9c27786a3fca613b8d6f32cf5ce69d0194c86c1f
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-09-16 09:40:25 +0900

    feat: 🎸 phpenv 用のパッケージをインストールするためのスクリプトを追加した

---
+++ system_global/install_os_packages/install_for_phpenv_build_lib_ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -xe
+
+sudo apt install -y libjpeg-dev libonig-dev libzip-dev libcurl4-openssl-dev re2c libxml2-dev libtidy-dev libxslt-dev libmcrypt-dev libreadline-dev autoconf
+
+exit 0
