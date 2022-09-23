0c559537e74a2f1ad65e4767f4c50e7d7c7b1db3
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-09-24 07:11:41 +0900

    feat: 🎸 ngrok のインストール手引きを追加した

---
+++ install_ngrok.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "https://dashboard.ngrok.com/get-started/setup にアクセスしてインストールする"
+echo "$ cd /tmp && wget https://*/ngrok-v3-stable-linux-amd64.tgz"
+echo "$ unar ngrok-*"
+echo "$ ls -la ngrok*"
+echo "$ mv ngrok TARGET_PATH"
+
+exit 0
