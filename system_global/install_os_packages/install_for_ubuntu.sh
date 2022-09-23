a6ceacb03b4dcc9754fef8e43be263d4c0479e20
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-09-24 07:29:00 +0900

    feat: 🎸 rsync は Ubuntu ミニマム のときには入っていないこともあるので追加

--- system_global/install_os_packages/install_for_ubuntu.sh
+++ system_global/install_os_packages/install_for_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt install -y gh peco direnv expect neovim unar rclone
+sudo apt install -y rsync gh peco direnv expect neovim unar rclone
 sudo apt install -y libmysqlclient-dev mysql-client
 sudo apt install -y libpq-dev postgresql-client
 
