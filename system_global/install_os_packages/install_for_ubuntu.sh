1233a3131a148ea06d2d8663679404278c92831f
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-09-24 08:13:52 +0900

    feat: 🎸 日本語フォントのインストールスクリプトを追加した

--- system_global/install_os_packages/install_for_ubuntu.sh
+++ system_global/install_os_packages/install_for_ubuntu.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt install -y rsync gh peco direnv expect neovim unar rclone
+sudo apt install -y dialog rsync
+sudo apt install -y locales fonts-ipafont fonts-ipaexfont
+sudo apt install -y gh peco direnv expect neovim unar rclone
 sudo apt install -y libmysqlclient-dev mysql-client
 sudo apt install -y libpq-dev postgresql-client
 
