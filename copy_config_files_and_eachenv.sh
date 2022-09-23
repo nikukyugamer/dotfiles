8cd2a5f37362e973cde938fa5d1c91d0416a54b1
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-09-23 14:41:57 +0000

    feat: 🎸 設定ファイルのコピースクリプトを微修正した

--- copy_config_files_and_eachenv.sh
+++ copy_config_files_and_eachenv.sh
@@ -1,9 +1,12 @@
-#!/bin/bash -xe
+#!/usr/bin/env bash
 
 cp .zshrc.eachenv.should_cp_to_home_directory.zshrc ~/.zshrc
+
+# 既存のファイルがあった場合に上書きしないようにする
 cp .zshrc.userown.zshrc ~/.zshrc.userown.zshrc.sample
-cp .vimrc.should_cp_to_home_directory ~/.vimrc
+cp .vimrc.should_cp_to_home_directory ~/.vimrc.sample
 cp .gitconfig.should_cp_to_home_directory ~/.gitconfig.sample
+
 # cp .czrc ~/.czrc
 
 exit 0
