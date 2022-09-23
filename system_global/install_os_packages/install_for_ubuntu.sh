2adfa0ef6c49618c0dd1ba61bdd2808e124dbfbc
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-09-24 05:54:07 +0900

    feat: 🎸 install_for_ubuntu に微追加

--- system_global/install_os_packages/install_for_ubuntu.sh
+++ system_global/install_os_packages/install_for_ubuntu.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt install -y gh peco direnv expect neovim
+sudo apt install -y gh peco direnv expect neovim unar rclone
+sudo apt install -y libmysqlclient-dev mysql-client
+sudo apt install -y libpq-dev postgresql-client
 
 exit 0
