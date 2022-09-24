e37164bbbb943cda416e50e8ab14271ecee4c135
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-09-24 14:45:49 +0900

    feat: 🎸 WSL2 ではエラーが出るコマンドをコメントアウト

--- system_global/install_os_packages/init_japanese_ubuntu.sh
+++ system_global/install_os_packages/init_japanese_ubuntu.sh
@@ -3,8 +3,9 @@
 set -euxo pipefail
 cd "$(dirname "$0")"
 
-sudo update-locale LANG=ja_JP.UTF-8
-sudo timedatectl set-timezone Asia/Tokyo
 sudo apt install -y language-pack-ja language-pack-ja-base manpages-ja manpages-ja-dev
+sudo update-locale LANG=ja_JP.UTF-8
+# WSL2 だとエラーが出る
+# sudo timedatectl set-timezone Asia/Tokyo
 
 exit 0
