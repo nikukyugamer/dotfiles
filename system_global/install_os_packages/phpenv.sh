46777fa8fab3d2cd4d7f80f6780bf07bfeaef602
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-09-24 15:11:07 +0900

    refactor: 💡 install_os_packages 配下のスクリプトをリファクタリングした

--- system_global/install_os_packages/phpenv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt install -y bison re2c libxml2-dev libssl-dev libbz2-dev libcurl4-openssl-dev libjpeg-dev libpng-dev libreadline-dev libtidy-dev libxslt1-dev libzip-dev autoconf
-
-exit 0
