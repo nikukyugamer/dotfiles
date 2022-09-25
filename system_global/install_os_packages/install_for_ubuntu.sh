9ae61c51f52a7f2bdaf8c68232e6082c15d15176
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-09-25 09:53:50 +0900

    feat: 🎸 minimun ubuntu に入れるパッケージに unzip を追加した

--- system_global/install_os_packages/install_for_ubuntu.sh
+++ system_global/install_os_packages/install_for_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt install -y dialog rsync zsh
+sudo apt install -y dialog rsync zsh unzip
 sudo apt install -y locales fonts-ipafont fonts-ipaexfont
 sudo apt install -y gh peco direnv expect neovim unar rclone
 sudo apt install -y libmysqlclient-dev mysql-client
