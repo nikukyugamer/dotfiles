723314e05011c7d7dcd80ab73facaa876b998ccd
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-09-26 07:58:32 +0900

    feat: 🎸 git コマンド のビルド時に必要なパッケージに対応した

--- system_global/install_os_packages/install_for_ubuntu.sh
+++ system_global/install_os_packages/install_for_ubuntu.sh
@@ -7,4 +7,10 @@ sudo apt install -y gh peco direnv expect neovim unar rclone
 sudo apt install -y libmysqlclient-dev mysql-client
 sudo apt install -y libpq-dev postgresql-client
 
+# git コマンド をビルドする際に必要になる
+sudo apt install -y gettext asciidoc docbook2x libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
+
+echo
+echo "環境によっては $ pip install asciidoc が必要になる"
+
 exit 0
