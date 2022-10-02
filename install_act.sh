64c7fcba6f24d715c8cf09c44821337300b185a3
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-10-02 12:15:13 +0900

    feat: 🎸 act のインストーラをより適切にした

--- install_act.sh
+++ install_act.sh
@@ -1,7 +1,12 @@
-#!/bin/bash -xe
+#!/usr/bin/env bash
+set -euxo pipefail
+cd "$(dirname "$0")"
 
 # https://github.com/nektos/act
 # Run your GitHub Actions locally! Why would you want to do this? Two reasons:
+mkdir -p $HOME/bin
+cd $HOME
+
 curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
 
 exit 0
