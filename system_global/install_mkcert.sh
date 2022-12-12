b1eb4aa2eff0a6a634014d1d5fa97d8ae7cd3311
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-12-12 21:34:19 +0900

    feat: 🎸 mkcert のインストールスクリプトを追加した

---
+++ system_global/install_mkcert.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# mkcert is a simple tool for making locally-trusted development certificates.
+# https://github.com/FiloSottile/mkcert
+
+curl -JLO "https://dl.filippo.io/mkcert/latest?for=linux/amd64"
+chmod +x mkcert-v*-linux-amd64
+sudo cp mkcert-v*-linux-amd64 /usr/local/bin/mkcert
+rm mkcert-v*-linux-amd64
+
+exit 0
