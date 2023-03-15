1248754201df1613a79f32501dc457a0cca0d2d7
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-03-15 11:58:38 +0900

    feat: 🎸 Silicon に必要なライブラリを更新した

--- system_global/install_os_packages/install_for_silicon_ubuntu.sh
+++ system_global/install_os_packages/install_for_silicon_ubuntu.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt install -y expat libxml2-dev pkg-config libasound2-dev libssl-dev cmake libfreetype6-dev libexpat1-dev libxcb-composite0-dev
+sudo apt install -y expat
+sudo apt install -y libxml2-dev
+sudo apt install -y pkg-config libasound2-dev libssl-dev cmake libfreetype6-dev libexpat1-dev libxcb-composite0-dev libharfbuzz-dev
 
 exit 0
