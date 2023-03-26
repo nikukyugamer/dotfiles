e025974cb787f780b8825a92c0d0ec4128ba21c0
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-03-26 12:33:41 +0900

    feat: 🎸 Neovim の設定ファイルの場所が誤っていたので修正

--- system_global/install_neovim.sh
+++ system_global/install_neovim.sh
@@ -6,12 +6,12 @@ sudo apt install -y neovim
 
 echo
 echo "やること その1"
-echo "~/.config/nvim にエイリアスを作ること"
-echo "$ ln -s ~/.vimrc ~/.config/nvim"
+echo "~/.config/nvim/init.vim にエイリアスを作ること"
+echo "$ mkdir -p ~/.config/nvim && ln -s ~/.vimrc ~/.config/nvim/init.vim"
 
 echo
 echo "やること その2"
 echo ".zshrc.userown.zshrc などにエイリアスを作ること"
-echo "alias vim=/usr/bin/nvim"
+echo 'echo "alias vim=/usr/bin/nvim" >> ~/.zshrc.userown.zshrc'
 
 exit 0
