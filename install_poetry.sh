aab7ef76bddbd00c59d182d570107654da8bb67d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-04-25 09:28:55 +0900

    docs: ✏️ Poetry のインストールスクリプトにコメントを追加した

--- install_poetry.sh
+++ install_poetry.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+# Linux の場合、インストールされる場所は以下になる
+# ~/.local/bin
+# ~/.local/share/pypoetry
+
 curl -sSL https://install.python-poetry.org | python -
 
 exit 0
