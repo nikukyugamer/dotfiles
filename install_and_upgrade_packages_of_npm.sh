bab5fdbc90da71463e0f15bd98888ad79d8806ac
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-05-18 09:00:48 +0900

    feat: 🎸 aicommits のインストールスクリプトを追加した
    https://github.com/Nutlope/aicommits

--- install_and_upgrade_packages_of_npm.sh
+++ install_and_upgrade_packages_of_npm.sh
@@ -45,6 +45,7 @@ npm_install_g @amanoese/muscular
 npm_install_g @devcontainers/cli
 npm_install_g @fand/kao
 npm_install_g @google/clasp
+npm_install_g aicommits
 npm_install_g crontab-ui
 npm_install_g diff-so-fancy
 npm_install_g dsstore-cli
