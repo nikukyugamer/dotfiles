d6ccf735c7dabebb711e996f9072dbaef63f4de6
Osamu Takiya <takiya@toran.sakura.ne.jp>
2024-05-17 06:24:27 +0900

    feat: 🎸 Helm のインストールスクリプトを追加した
    https://helm.sh/

---
+++ install_helm_ubuntu.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# visudo にて %sudo ALL=NOPASSWD: /usr/bin/tee が必要になる
+
+# https://helm.sh/docs/intro/install/
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt install -y apt-transport-https
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt update
+sudo apt install -y helm
+
+exit 0
