e9f588e5d617a6b62d5b94e12117177336d7db3e
Osamu Takiya <takiya@toran.sakura.ne.jp>
2024-05-19 17:07:57 +0900

    feat: 🎸 $ composer diagnose 用に 7-Zip のインストールスクリプトを追加した

--- system_global/install_os_packages/install_for_phpenv_build_lib_ubuntu.sh
+++ system_global/install_os_packages/install_for_phpenv_build_lib_ubuntu.sh
@@ -3,4 +3,7 @@ set -xe
 
 sudo apt install -y libjpeg-dev libonig-dev libzip-dev libcurl4-openssl-dev re2c libxml2-dev libtidy-dev libxslt-dev libmcrypt-dev libreadline-dev autoconf
 
+# $ composer diagnose 用
+sudo apt install -y p7zip-full
+
 exit 0
