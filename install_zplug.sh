87765a08e11fdda7ee57e021ab4b863122c109fb
Osamu Takiya <takiya@toran.sakura.ne.jp>
2024-05-21 08:24:30 +0900

    install_zplug.sh の対話表示コメントをより詳しい内容に変更した

--- install_zplug.sh
+++ install_zplug.sh
@@ -3,6 +3,6 @@ set -e
 
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
-echo "インストールした後は '$ zplug install' を手動で実行して下さい"
+echo "インストールした後は '$ source ~/.zshrc' した後に '$ zplug install' を実行して下さい。"
 
 exit 0
