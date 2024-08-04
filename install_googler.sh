8860544b79364e2b600a3b796c36c601dc3f3213
Osamu Takiya <takiya@toran.sakura.ne.jp>
2024-08-04 17:51:32 +0900

    feat: 🎸 Obsolete となった "googler" のインストールスクリプトを削除した

--- install_googler.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-cd "$(dirname "$0")"
-
-sudo curl -o /usr/local/bin/googler https://raw.githubusercontent.com/jarun/googler/v4.3.2/googler && sudo chmod +x /usr/local/bin/googler
-
-exit 0
