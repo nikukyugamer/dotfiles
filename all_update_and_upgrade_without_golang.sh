2fefd2c47d113f383e5d66cdecb364b68216785a
Osamu Takiya <takiya@toran.sakura.ne.jp>
2024-08-22 13:34:12 +0900

    feat: 🎸 "all_update_and_upgrade" のスクリプトに Rye のスクリプトを組み込むなどして更新

--- all_update_and_upgrade_without_golang.sh
+++ all_update_and_upgrade_without_golang.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
 # install_and_upgrade_packages
-$HOME/dotfiles/install_and_upgrade_packages_of_pip.sh
-$HOME/dotfiles/install_and_upgrade_packages_of_rust.sh
-$HOME/dotfiles/install_and_upgrade_packages_of_npm.sh
+"$HOME"/dotfiles/install_and_upgrade_packages_of_rye_tools_install.sh
+"$HOME"/dotfiles/install_and_upgrade_packages_of_pip.sh
+"$HOME"/dotfiles/install_and_upgrade_packages_of_rust.sh
+"$HOME"/dotfiles/install_and_upgrade_packages_of_npm.sh
 
 # update_scripts
-$HOME/dotfiles/update_scripts/update_rbenv_nodenv_pyenv_goenv_jenv_phpenv.sh
-$HOME/dotfiles/update_scripts/update_fzf.sh
-$HOME/dotfiles/update_scripts/update_cli_tools.sh
-$HOME/dotfiles/update_scripts/update_rust.sh
+"$HOME"/dotfiles/update_scripts/update_rbenv_nodenv_pyenv_goenv_jenv_phpenv.sh
+"$HOME"/dotfiles/update_scripts/update_fzf.sh
+"$HOME"/dotfiles/update_scripts/update_cli_tools.sh
+"$HOME"/dotfiles/update_scripts/update_rust.sh
 
-$HOME/dotfiles/install_and_update_deno.sh
-$HOME/dotfiles/install_pnpm.sh
-$HOME/dotfiles/install_bun.sh
+"$HOME"/dotfiles/install_and_update_deno.sh
+"$HOME"/dotfiles/install_and_update_pnpm.sh
+"$HOME"/dotfiles/install_and_update_bun.sh
 
 exit 0
