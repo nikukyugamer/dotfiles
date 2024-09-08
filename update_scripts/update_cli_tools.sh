d3d2036cdd59eb9824354aca1e8c3fe874b54cde
Osamu Takiya <takiya@toran.sakura.ne.jp>
2024-09-08 17:50:22 +0900

    refactor: 💡 update_scripts/ を中心にリファクタした (#293)

--- update_scripts/update_cli_tools.sh
+++ update_scripts/update_cli_tools.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-set -e pipefail
+set -euxo pipefail
+cd "$(dirname "$0")"
 
-UNAME=`uname -a`
+UNAME=$(uname -a)
 
-if [ "`echo $UNAME | grep arm`" ]; then
+if echo "$UNAME" | grep -q arm; then
   # arm では heroku update は 404エラー になる
   echo 'arm: Heroku CLI の アップデート をスキップします'
-elif [ "`echo $UNAME | grep aarch64`" ]; then
+elif echo "$UNAME" | grep -q aarch64; then
   # arm では heroku update は 404エラー になる
   echo 'aarch64: Heroku CLI の アップデート をスキップします'
 else
@@ -14,21 +15,21 @@ else
   heroku plugins:install heroku-accounts
 fi
 
-if [ "`echo $UNAME | grep Darwin`" ]; then
+if echo "$UNAME" | grep -q Darwin; then
   echo 'Update circleci...'
   circleci update
-elif [ "`echo $UNAME | grep arm`" ]; then
+elif echo "$UNAME" | grep -q arm; then
   # 現状、arm では動作しない
   # circleci update
   echo 'Skip "circleci update"'
-elif [ "`echo $UNAME | grep aarch64`" ]; then
+elif echo "$UNAME" | grep -q aarch64; then
   # 現状、arm では動作しない
   # circleci update
   echo 'aarch64: Skip "circleci update"'
-elif [ "`echo $UNAME | grep Ubuntu`" ]; then
+elif echo "$UNAME" | grep -q Ubuntu; then
   echo 'Update circleci...'
   circleci update
-elif [ "`echo $UNAME | grep el7`" ]; then
+elif echo "$UNAME" | grep -q el7; then
   echo 'Update circleci...'
   circleci update
 else
