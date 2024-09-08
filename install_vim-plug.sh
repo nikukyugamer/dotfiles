59ea8f60356bb843cdeb209f4ff8395fd3d81b44
Osamu Takiya <takiya@toran.sakura.ne.jp>
2024-09-08 10:36:35 +0900

    feat: 🎸 vim-plug のインストールスクリプトを追加し rainbow_csv をプラグインとして設定した

---
+++ install_vim-plug.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# 公式リポジトリ
+# https://github.com/junegunn/vim-plug
+
+# Neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Vim
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo 'インストールをした後は Neovim を起動し、":PlugInstall" を実行して下さい。'
+
+exit 0
