17c5bc23c73e81b2ed79332d1ae0f04fef0bb611
Osamu Takiya <takiya@toran.sakura.ne.jp>
2024-09-26 06:46:12 +0900

    fix: 🐛 Rye のアップデート用スクリプトを修正した

--- update_scripts/update_rye.sh
+++ update_scripts/update_rye.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # https://rye.astral.sh/guide/installation/
 
-# インストール時と同じスクリプトを実行すればアップデートできる
-"$HOME"/dotfiles/initial_scripts_and_settings/scripts/install_rye.sh
+rye self update
+
+echo "Python のバージョン一覧を確認するためには $ rye toolchain list --include-downloadable を実行する。"
+echo "新しい Python のバイナリをインストールするためには $ rye toolchain fetch 3.12.6 を実行する。"
+echo "既存の Python のバイナリをアンインストールするためには $ rye toolchain remove --force 3.12.5 を実行する。"
+echo "グローバルの Python のバージョンを変更するには ~/.rye/config.toml を編集する。"
 
 exit 0
