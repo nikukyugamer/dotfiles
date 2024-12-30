e0c7d43dbe367e6af0b7588ca15ef0914bb86fe8
Osamu Takiya <takiya@toran.sakura.ne.jp>
2024-12-30 10:40:17 +0900

    feat: 🎸 Add Yazi installation script (#339)

--- initial_scripts_and_settings/scripts/install_packages_of_rust.sh
+++ initial_scripts_and_settings/scripts/install_packages_of_rust.sh
@@ -54,6 +54,7 @@ install_cargo simple-http-server
 install_cargo tealdeer
 install_cargo tokei # https://github.com/XAMPPRocky/tokei
 install_cargo xcompress
+install_cargo yazi-fm yazi-cli # https://yazi-rs.github.io/
 
 # https://github.com/dathere/qsv
 # "--features python" は Python のバージョンによりインストールに失敗するので原則として外すことにする
