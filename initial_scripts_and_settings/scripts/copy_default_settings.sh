f80f295eb6863a20f52c10bb3c13991fd384889c
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-01-08 22:23:21 +0900

    feat: 🎸 Visual Studio Code の Jest 用に .zshenv を追加した (#344)

--- initial_scripts_and_settings/scripts/copy_default_settings.sh
+++ initial_scripts_and_settings/scripts/copy_default_settings.sh
@@ -5,6 +5,7 @@ cd "$(dirname "$0")"
 cp .zshrc.to_home_directory.zshrc "$HOME"/.zshrc
 
 # 以下はカスタマイズ前提のファイル群なので、既存のファイルがあった場合に上書きしないように一時的な名前でコピーする
+cp "$HOME"/dotfiles/.zshenv "$HOME"/.zshenv.default
 cp "$HOME"/dotfiles/initial_scripts_and_settings/settings/.zshrc.userown.zshrc "$HOME"/.zshrc.userown.zshrc.default
 cp "$HOME"/dotfiles/initial_scripts_and_settings/settings/.vimrc.should_cp_to_home_directory.vimrc "$HOME"/.vimrc.default
 cp "$HOME"/dotfiles/initial_scripts_and_settings/settings/.gitconfig.should_cp_to_home_directory.gitconfig "$HOME"/.gitconfig.default
