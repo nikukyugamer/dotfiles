3da36202b5596947e11d3f622b342dd453197214
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-01-25 21:34:58 +0900

    fix: 🐛 copy_default_settings.sh 内のコピー対象ファイルのパスが変更になったので修正した

--- initial_scripts_and_settings/scripts/copy_default_settings.sh
+++ initial_scripts_and_settings/scripts/copy_default_settings.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 cd "$(dirname "$0")"
 
-cp .zshrc.to_home_directory.zshrc "$HOME"/.zshrc
+cp "$HOME/dotfiles/initial_scripts_and_settings/settings/.zshrc.to_home_directory.zshrc" "$HOME"/.zshrc
 
 # 以下はカスタマイズ前提のファイル群なので、既存のファイルがあった場合に上書きしないように一時的な名前でコピーする
 cp "$HOME"/dotfiles/.zshenv "$HOME"/.zshenv.default
