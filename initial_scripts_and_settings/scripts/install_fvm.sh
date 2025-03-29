6205d138f79f9af3306b4f9cc234cf51f3ce43e6
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-03-29 12:23:50 +0900

    feat: 🎸 fvm のインストールスクリプトを更新した (#382)

--- initial_scripts_and_settings/scripts/install_fvm.sh
+++ initial_scripts_and_settings/scripts/install_fvm.sh
@@ -2,9 +2,25 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
-# macOS だと /usr/local/bin/fvm にシンボリックリンクを入れようとして sudo が必要になる
-# なので、面倒だから Homebrew で入れた方が良い（公式ドキュメント参照）
-# https://fvm.app/documentation/getting-started/installation
-curl -fsSL https://fvm.app/install.sh | bash
+echo "fvm は Homebrew で入れることを推奨する"
+echo "ただしその場合は定期的に brew update を実行する必要がある"
+echo
+echo "Homebrew は Ubuntu でも入れられる (https://brew.sh/ja/)"
+echo "Ubuntu の場合は /home/linuxbrew というユーザが作られて、その配下を参照する形になる"
+echo
+echo "cf. https://fvm.app/documentation/getting-started/installation"
+echo "$ brew tap leoafarias/fvm"
+echo "$ brew install fvm"
+echo
+echo "install.sh でのインストールは推奨しない"
+echo "cf. https://github.com/leoafarias/fvm/pull/792"
+echo "cf. https://github.com/leoafarias/fvm/issues/796"
+echo
+echo "インストール後はシェルの設定ファイルに環境変数を設定すること"
+echo "# Flutter (Use fvm)"
+echo "export FLUTTER_HOME=$HOME/fvm/default"
+echo "export PATH=$PATH:$FLUTTER_HOME/bin"
+
+# curl -fsSL https://fvm.app/install.sh | bash
 
 exit 0
