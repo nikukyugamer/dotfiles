f632a18dc8605e137cd69c1bd79379f2dca71591
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-03-29 17:35:15 +0900

    feat: 🎸 Rust のパッケージの インストーラ & アップデータ を大きく刷新した

--- initial_scripts_and_settings/scripts/install_packages_of_rust.sh
+++ initial_scripts_and_settings/scripts/install_packages_of_rust.sh
@@ -1,83 +1,122 @@
 #!/usr/bin/env bash
 
-function install_cargo() {
+function install_cargo_first_time() {
   echo '======================================================='
-
+  echo "[LOG (START)] $ cargo install $1 --locked"
   echo '======================================================='
-
   cargo install "$1" --locked
-
   echo '======================================================='
-  echo "[LOG] END: $ cargo install $1"
-  echo '======================================================='
-}
-
-function cargo_install_update_all() {
-  echo '======================================================='
-  echo "[LOG] START: $ cargo install-update --all"
+  echo "[LOG (END)] $ cargo install $1 --locked"
   echo '======================================================='
 
-  cargo install-update --all
-
-  echo '======================================================='
-  echo "[LOG] END: $ cargo install-update --all"
-  echo '======================================================='
-
-  echo '**********************************************************************'
-}
-
-install_cargo cargo-update
-cargo_install_update_all
-
-cargo install --git https://github.com/Peltoche/lsd.git --branch master # https://github.com/Peltoche/lsd
-
-install_cargo ag
-install_cargo bat
-install_cargo bottom
-install_cargo broot
-install_cargo diskonaut
-install_cargo du-dust
-install_cargo exa
-install_cargo fd-find
-install_cargo git-delta
-install_cargo git-interactive-rebase-tool
-install_cargo gitui # https://github.com/extrawurst/gitui
-install_cargo grex
-install_cargo hyperfine
-install_cargo kiro-editor
-install_cargo macchina # https://github.com/Macchina-CLI/macchina
-install_cargo procs
-install_cargo railwayapp
-install_cargo ripgrep
-install_cargo ripgrep_all # ffmpeg などが必要
-install_cargo sd          # Alt "sed" https://github.com/chmln/sd
-install_cargo simple-http-server
-install_cargo tailspin # https://github.com/bensadeh/tailspin (A log file highlighter)
-install_cargo tealdeer
-install_cargo tokei # https://github.com/XAMPPRocky/tokei
-install_cargo xcompress
-install_cargo xh # https://github.com/ducaale/xh
-install_cargo yazi-fm yazi-cli # https://yazi-rs.github.io/
-
-# https://github.com/dathere/qsv
-# "--features python" オプションは、Python のバージョンによってはインストールに失敗するので原則として外すことにする
-
-# NOTE: 一時的対応
-# 依存関係の影響上、インストールに失敗するため、https://github.com/YS-L/csvlens のバージョンでインストール可否を判別する
-# cf. https://github.com/dathere/qsv/issues/2543
-# cf. https://github.com/dathere/qsv/issues/2574
-# cf. https://github.com/dathere/qsv/issues/2582
-
-csvlens_version=$(cargo search csvlens | grep "csvlens = " | sed -E "s/csvlens = \"([0-9]+\.[0-9]+\.[0-9]+)\".*/\1/")
-if [ "$csvlens_version" != "0.12.0" ]; then
   echo
-  echo 'Install "qsv"'
-  cargo install qsv --locked --bin qsv --features feature_capable,apply,fetch,foreach,geocode,luau,polars,self_update,ui
-fi
+}
 
-# tealdeer を更新する
+function cargo_install_update() {
+  local package_name="$1"
+
+  echo '======================================================='
+  echo "[LOG (START)] $ cargo install-update $package_name"
+  echo '======================================================='
+  cargo install-update "$package_name"
+  echo '======================================================='
+  echo "[LOG (END)] $ cargo install-update $package_name"
+  echo '======================================================='
+
+  echo
+}
+
+# 複数の箇所で用いるため、対象のパッケージ名を配列で持つ
+# 追加や削除はここに対して行う
+TARGET_PACKAGE_NAMES=(
+  ag
+  bat
+  bottom
+  broot
+  cargo-update
+  diskonaut
+  du-dust
+  exa
+  fd-find
+  git-delta
+  git-interactive-rebase-tool
+  gitui # https://github.com/extrawurst/gitui
+  grex
+  hyperfine
+  kiro-editor
+  macchina # https://github.com/Macchina-CLI/macchina
+  procs
+  # https://github.com/dathere/qsv
+  # "--features python" オプションは、Python のバージョンによってはインストールに失敗するので原則として外すことにする
+  # qsv
+  railwayapp
+  ripgrep
+  ripgrep_all # ffmpeg などが必要
+  sd # Alt "sed" https://github.com/chmln/sd
+  simple-http-server
+  tailspin # https://github.com/bensadeh/tailspin (A log file highlighter)
+  tealdeer
+  tokei # https://github.com/XAMPPRocky/tokei
+  xcompress
+  xh # https://github.com/ducaale/xh
+  yazi-fm
+  yazi-cli # https://yazi-rs.github.io/
+)
+
+# 現在のインストール状況の一覧を表示する
+$(which cargo) install-update --list
 echo
-echo 'Update "tealdeer (tldr)"'
-tldr --update
+
+# 初期インストールを実行する
+# TODO: すでにインストールされている場合は時間の無駄になるのでスキップしたい
+for package_name in "${TARGET_PACKAGE_NAMES[@]}"; do
+  install_cargo_first_time "$package_name"
+done
+
+# アップデート時に実行する
+for package_name in "${TARGET_PACKAGE_NAMES[@]}"; do
+  # tealdeer は CLI から直接更新する
+  if [ "$package_name" = "tealdeer" ]; then
+    continue
+  fi
+
+  if [ "$package_name" = "qsv" ]; then
+    # NOTE: 一時的対応
+    # 依存関係の影響上、インストールに失敗するため、https://github.com/YS-L/csvlens のバージョンでインストール可否を判別する
+    # cf. https://github.com/dathere/qsv/issues/2543
+    # cf. https://github.com/dathere/qsv/issues/2574
+    # cf. https://github.com/dathere/qsv/issues/2582
+    CSVLENS_VERSION=$(cargo search csvlens | grep "csvlens = " | sed -E "s/csvlens = \"([0-9]+\.[0-9]+\.[0-9]+)\".*/\1/")
+    if [ "$CSVLENS_VERSION" != "0.12.0" ]; then
+      echo
+      echo 'Install "qsv"'
+      cargo install qsv --locked --bin qsv --features feature_capable,apply,fetch,foreach,geocode,luau,polars,self_update,ui
+    fi
+  fi
+
+  cargo_install_update "$package_name"
+done
+
+# tealdeer は CLI から直接更新する
+echo
+echo '======================================================='
+echo '[LOG (START)] Update tealdeer "$ tldr --update"'
+echo '======================================================='
+$(which tldr) --update
+echo '======================================================='
+echo '[LOG (END)] Update tealdeer "$ tldr --update"'
+echo '======================================================='
+echo
+
+# lsd は直接リポジトリを指定してインストールする必要がある
+echo
+echo '======================================================='
+echo '[LOG (START)] Install or Update "lsd"'
+echo '======================================================='
+cargo install --git https://github.com/Peltoche/lsd.git --branch master # https://github.com/Peltoche/lsd
+echo '======================================================='
+echo '[LOG (END)] Install or Update "lsd"'
+echo '======================================================='
+echo
 
 exit 0
