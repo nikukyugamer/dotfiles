771ee98fdfc6677f06619ec210d397c3d6e9f322
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-04-17 17:33:02 +0900

    feat: 🎸 Ubuntu にほぼ必須の cmake パッケージ のインストーラを追記した

--- system_global/install_os_packages/install_for_ubuntu.sh
+++ system_global/install_os_packages/install_for_ubuntu.sh
@@ -8,6 +8,9 @@ sudo apt install -y gh peco direnv expect neovim unar rclone
 sudo apt install -y libmysqlclient-dev mysql-client
 sudo apt install -y libpq-dev postgresql-client
 
+# ビルド用ライブラリは rbenv などに主要なものが入っているのでそちらも入れること
+sudo apt install -y cmake
+
 # git コマンド をビルドする際に必要になる
 sudo apt install -y gettext asciidoc docbook2x libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
 
