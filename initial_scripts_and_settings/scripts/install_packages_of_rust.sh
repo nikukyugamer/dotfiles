3cffbd980ddf217b6439d3095559026f1abfc0a2
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-05-20 18:10:23 +0900

    feat: 🎸 cargo のインストールスクリプトを大幅刷新した

--- initial_scripts_and_settings/scripts/install_packages_of_rust.sh
+++ initial_scripts_and_settings/scripts/install_packages_of_rust.sh
@@ -2,16 +2,32 @@
 
 function install_cargo_first_time() {
   echo '======================================================='
-  echo "[LOG (START)] $ cargo install $1 --locked"
+  echo "[LOG (START)] $ cargo install $1"
   echo '======================================================='
-  cargo install "$1" --locked
+  cargo install "$1"
   echo '======================================================='
-  echo "[LOG (END)] $ cargo install $1 --locked"
+  echo "[LOG (END)] $ cargo install $1"
   echo '======================================================='
 
   echo
 }
 
+# "$1" は "URL バイナリ名" というテキストである
+function install_cargo_first_time_via_git() {
+  echo '======================================================='
+  echo "[LOG (START)] $ cargo install --git $1"
+  echo '======================================================='
+  # SC2086: Double quote to prevent globbing and word splitting
+  # shellcheck disable=SC2086
+  cargo install --git $1
+  echo '======================================================='
+  echo "[LOG (END)] $ cargo install --git $1"
+  echo '======================================================='
+
+  echo
+}
+
+# "cargo-update" を用いてアップデートする
 function cargo_install_update() {
   local package_name="$1"
 
@@ -24,6 +40,18 @@ function cargo_install_update() {
   echo '======================================================='
 
   echo
+}
+
+# パッケージがすでにインストールされているかをチェックする関数
+function is_cargo_package_installed() {
+  local package_name="$1"
+
+  # cargo install --list の出力から該当パッケージがインストール済みかチェック
+  if cargo install --list | grep -q "^$package_name "; then
+    return 0  # インストール済み
+  else
+    return 1  # 未インストール
+  fi
 }
 
 # 複数の箇所で用いるため、対象のパッケージ名を配列で持つ
@@ -46,14 +74,12 @@ TARGET_PACKAGE_NAMES=(
   kiro-editor
   macchina # https://github.com/Macchina-CLI/macchina
   procs
-  # https://github.com/dathere/qsv
-  # "--features python" オプションは、Python のバージョンによってはインストールに失敗するので原則として外すことにする
-  # qsv
   railwayapp
   ripgrep
-  ripgrep_all # ffmpeg などが必要
+  ripgrep_all # ffmpeg などがインストールされていることが前提となる
   sd # Alt "sed" https://github.com/chmln/sd
   simple-http-server
+  starship
   tailspin # https://github.com/bensadeh/tailspin (A log file highlighter)
   tealdeer
   tokei # https://github.com/XAMPPRocky/tokei
@@ -63,61 +89,41 @@ TARGET_PACKAGE_NAMES=(
   yazi-cli # https://yazi-rs.github.io/
 )
 
-# 現在のインストール状況の一覧を表示する
-$(which cargo) install-update --list
-echo
+# "URL バイナリ名" というテキストを配列で持つ
+# ここに該当するバイナリは cargo install によりアップデートされるので cargo-update は不要（のはず）
+TARGET_PACKAGE_URL_AND_BIN_NAME_STRINGS=(
+  "https://github.com/astral-sh/uv uv"
+)
 
-# 初期インストールを実行する
-# TODO: すでにインストールされている場合は時間の無駄になるのでスキップしたい
+# 現在のインストール状況の一覧を表示する
+echo '現在のインストール状況の一覧を表示する'
+$(which cargo) install-update --list
+
+# 初期インストールコマンドを実行する
 for package_name in "${TARGET_PACKAGE_NAMES[@]}"; do
-  install_cargo_first_time "$package_name"
+  # インストールされていない場合のみ、インストールコマンドを実行する
+  if ! is_cargo_package_installed "$package_name"; then
+    install_cargo_first_time "$package_name"
+  fi
 done
 
-# アップデート時に実行する
+# 初期インストールコマンドを実行する（"--git" の場合）
+for url_and_bin_name in "${TARGET_PACKAGE_URL_AND_BIN_NAME_STRINGS[@]}"; do
+  install_cargo_first_time_via_git "$url_and_bin_name"
+done
+
+# すでにインストールされているパッケージのアップデートを実行する
+# cf. https://github.com/nabijaczleweli/cargo-update
 for package_name in "${TARGET_PACKAGE_NAMES[@]}"; do
-  # tealdeer は CLI から直接更新する
-  if [ "$package_name" = "tealdeer" ]; then
-    continue
-  fi
-
-  if [ "$package_name" = "qsv" ]; then
-    # NOTE: 一時的対応
-    # 依存関係の影響上、インストールに失敗するため、https://github.com/YS-L/csvlens のバージョンでインストール可否を判別する
-    # cf. https://github.com/dathere/qsv/issues/2543
-    # cf. https://github.com/dathere/qsv/issues/2574
-    # cf. https://github.com/dathere/qsv/issues/2582
-    CSVLENS_VERSION=$(cargo search csvlens | grep "csvlens = " | sed -E "s/csvlens = \"([0-9]+\.[0-9]+\.[0-9]+)\".*/\1/")
-    if [ "$CSVLENS_VERSION" != "0.12.0" ]; then
-      echo
-      echo 'Install "qsv"'
-      cargo install qsv --locked --bin qsv --features feature_capable,apply,fetch,foreach,geocode,luau,polars,self_update,ui
-    fi
-  fi
-
   cargo_install_update "$package_name"
 done
 
-# tealdeer は CLI から直接更新する
-echo
-echo '======================================================='
-echo '[LOG (START)] Update tealdeer "$ tldr --update"'
-echo '======================================================='
-$(which tldr) --update
-echo '======================================================='
-echo '[LOG (END)] Update tealdeer "$ tldr --update"'
-echo '======================================================='
-echo
-
-# uv は直接リポジトリを指定してインストールする必要がある
-echo
-echo '======================================================='
-echo '[LOG (START)] Install or Update "uv"'
-echo '======================================================='
-# https://docs.astral.sh/uv/getting-started/installation/#cargo
-cargo install --git https://github.com/astral-sh/uv uv
-echo '======================================================='
-echo '[LOG (END)] Install or Update "uv"'
-echo '======================================================='
-echo
+# qsv は依存関係のバージョンの関係上、しばらくは下記コマンドでインストールする
+# 本来は feature_capable,apply,fetch,foreach,geocode,luau,polars,self_update,ui の --features でインストールしたい
+# "--features python" オプションは、Python のバージョンによってはインストールに失敗するので原則として外すことにする
+# cf. https://github.com/dathere/qsv/blob/master/docs/FEATURES.md
+# cf. https://github.com/YS-L/csvlens
+# cf. https://github.com/dathere/qsv/issues/2543, https://github.com/dathere/qsv/issues/2574, https://github.com/dathere/qsv/issues/2582
+cargo install qsv --locked --bin qsv --features feature_capable,apply,fetch,foreach,geocode,luau,self_update
 
 exit 0
