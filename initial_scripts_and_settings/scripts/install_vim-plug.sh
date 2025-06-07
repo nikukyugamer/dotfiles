57b0d411904c7c5d88f5799489d3f8864854cde8
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-06-07 19:10:15 +0900

    feat: 🎸 Update vim-plug installer (#437)

--- initial_scripts_and_settings/scripts/install_vim-plug.sh
+++ initial_scripts_and_settings/scripts/install_vim-plug.sh
@@ -4,11 +4,11 @@ set -euxo pipefail
 # 公式リポジトリ
 # https://github.com/junegunn/vim-plug
 
-# Neovim（今のところ、Neovim を使う前提としている）
+# Neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Vim
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo 'インストールをした後は Neovim を起動し、":PlugInstall" を実行して下さい。'
 
