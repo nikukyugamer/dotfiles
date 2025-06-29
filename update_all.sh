192ccb78dba376c14c448e0f7917a4e057a8a8c9
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-06-29 12:53:42 +0900

    feat: 🎸 Renewal v3

--- update_all.sh
+++ update_all.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 # CLI ツールのアップデート
-"$HOME"/dotfiles/update_scripts/update_rbenv_goenv_jenv_phpenv.sh
-"$HOME"/dotfiles/update_scripts/update_fzf.sh
+"$HOME"/dotfiles/update_scripts/cli_installers/update_rbenv_goenv_jenv_phpenv.sh
+"$HOME"/dotfiles/update_scripts/cli_installers/update_fzf.sh
 
 echo
 
 # パッケージのアップデート
-"$HOME"/dotfiles/update_scripts/update_packages_of_uv_tools.sh
-"$HOME"/dotfiles/update_scripts/update_packages_of_rust.sh
-"$HOME"/dotfiles/update_scripts/update_packages_of_npm.sh
-"$HOME"/dotfiles/update_scripts/update_packages_of_golang.sh
+"$HOME"/dotfiles/update_scripts/packages/update_packages_of_uv_tools.sh
+"$HOME"/dotfiles/update_scripts/packages/update_packages_of_rust.sh
+"$HOME"/dotfiles/update_scripts/packages/update_packages_of_npm.sh
+"$HOME"/dotfiles/update_scripts/packages/update_packages_of_golang.sh
 
 # 処理系のアップデート
-"$HOME"/dotfiles/update_scripts/update_bun.sh
-"$HOME"/dotfiles/update_scripts/update_deno.sh
-"$HOME"/dotfiles/update_scripts/update_pnpm.sh
-"$HOME"/dotfiles/update_scripts/update_rust.sh
+"$HOME"/dotfiles/update_scripts/cli_installers/update_bun.sh
+"$HOME"/dotfiles/update_scripts/cli_installers/update_deno.sh
+"$HOME"/dotfiles/update_scripts/cli_installers/update_pnpm.sh
+"$HOME"/dotfiles/update_scripts/cli_installers/update_rust.sh
 
 exit 0
