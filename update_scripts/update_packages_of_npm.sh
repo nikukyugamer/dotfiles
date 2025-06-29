bedc5de3c9e76a7b26820ab5b9e470749f8973bd
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-06-29 12:38:29 +0900

    feat: 🎸 Renewal v2 (#452)

--- update_scripts/update_packages_of_npm.sh
+++ update_scripts/update_packages_of_npm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # インストール時と同じスクリプトを実行すればアップデートできる
-"$HOME"/dotfiles/initial_scripts_and_settings/scripts/install_packages_of_npm.sh
+"$HOME"/dotfiles/initial_scripts_and_settings/scripts/packages/install_packages_of_npm.sh
 
 exit 0
--- update_scripts/update_packages_of_rust.sh
+++ update_scripts/update_packages_of_rust.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # インストール時と同じスクリプトを実行すればアップデートできる
-"$HOME"/dotfiles/initial_scripts_and_settings/scripts/install_packages_of_rust.sh
+"$HOME"/dotfiles/initial_scripts_and_settings/scripts/packages/install_packages_of_rust.sh
 
 exit 0
