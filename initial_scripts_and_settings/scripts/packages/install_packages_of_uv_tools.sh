5329e12799ef9e60940fe35a2299f4ab7b0bd997
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-07-01 19:04:00 +0900

    feat: 🎸 高速化のため不要となった uv tool install の対象をコメントアウトした (#463)

--- initial_scripts_and_settings/scripts/packages/install_packages_of_uv_tools.sh
+++ initial_scripts_and_settings/scripts/packages/install_packages_of_uv_tools.sh
@@ -17,30 +17,44 @@ function uv_tool_install() {
 uv_tool_install asciidoc               # git のビルド用
 uv_tool_install asciinema              # https://docs.asciinema.org/manual/
 uv_tool_install aws-encryption-sdk-cli # https://aws-encryption-sdk-cli.readthedocs.io/en/latest/
-uv_tool_install bottle
+# 2025/07/01 にコメントアウトしたので時期をおいて削除
+# uv_tool_install bottle
 uv_tool_install chat_downloader
-uv_tool_install cuteuid # https://github.com/alexdredmon/cuteuid
+# 2025/07/01 にコメントアウトしたので時期をおいて削除
+# uv_tool_install cuteuid # https://github.com/alexdredmon/cuteuid
 uv_tool_install glances
 uv_tool_install httpie
-uv_tool_install instaloader
+# 2025/07/01 にコメントアウトしたので時期をおいて削除
+# uv_tool_install instaloader
 uv_tool_install mycli
 uv_tool_install pgcli
-uv_tool_install psycopg2-binary
-uv_tool_install pysnmp
-uv_tool_install pyxattr
-uv_tool_install ranger-fm # ranger (CLI Filer)
+# 2025/07/01 にコメントアウトしたので時期をおいて削除
+# uv_tool_install psycopg2-binary
+# 2025/07/01 にコメントアウトしたので時期をおいて削除
+# uv_tool_install pysnmp
+# 2025/07/01 にコメントアウトしたので時期をおいて削除
+# uv_tool_install pyxattr
+# 2025/07/01 にコメントアウトしたので時期をおいて削除
+# uv_tool_install ranger-fm # ranger (CLI Filer)
 uv_tool_install sen       # https://github.com/TomasTomecek/sen
-uv_tool_install setuptools
-uv_tool_install sqruff
-uv_tool_install stormssh
+# 2025/07/01 にコメントアウトしたので時期をおいて削除
+# uv_tool_install setuptools
+uv_tool_install sqruff # https://github.com/quarylabs/sqruff (Fast SQL formatter/linter)
+# 2025/07/01 にコメントアウトしたので時期をおいて削除
+# uv_tool_install stormssh
 uv_tool_install streamlink
-uv_tool_install termdown
-uv_tool_install twitterscraper # https://github.com/taspinar/twitterscraper
-uv_tool_install twspace-dl     # https://github.com/HoloArchivists/twspace-dl
-uv_tool_install wad            # WAD (Web application detector)
+# 2025/07/01 にコメントアウトしたので時期をおいて削除
+# uv_tool_install termdown
+# 2025/07/01 にコメントアウトしたので時期をおいて削除
+# uv_tool_install twitterscraper # https://github.com/taspinar/twitterscraper
+# 2025/07/01 にコメントアウトしたので時期をおいて削除
+# uv_tool_install twspace-dl     # https://github.com/HoloArchivists/twspace-dl
+# 2025/07/01 にコメントアウトしたので時期をおいて削除
+# uv_tool_install wad            # WAD (Web application detector)
 uv_tool_install yamllint       # https://yamllint.readthedocs.io/en/stable/
 uv_tool_install youtube-comment-downloader
-uv_tool_install youtube-dl
+# 2025/07/01 にコメントアウトしたので時期をおいて削除
+# uv_tool_install youtube-dl
 uv_tool_install yt-dlp
 
 exit 0
